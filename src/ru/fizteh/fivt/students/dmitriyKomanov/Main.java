269d6c1b25d3808e8b9056dc0f494e7e26014fa7
Dmitriy Komanov <dkomanov@gmail.com>
2012-10-04 05:45:27 +0400

    Added title

--- src/ru/fizteh/fivt/students/dmitriyKomanov/Main.java
+++ src/ru/fizteh/fivt/students/dmitriyKomanov/Main.java
@@ -2,6 +2,7 @@ package ru.fizteh.fivt.students.dmitriyKomanov;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Arguments count: ");
         System.out.println(args.length);
     }
 }
