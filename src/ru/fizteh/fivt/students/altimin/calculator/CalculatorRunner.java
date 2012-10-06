fa43291bfc19f5232fc8faab674cda393346e92b
Timin Aleksandr <altimin@altimin-U36SD.(none)>
2012-10-06 17:48:35 +0400

    Space support fixed

--- src/ru/fizteh/fivt/students/altimin/calculator/CalculatorRunner.java
+++ src/ru/fizteh/fivt/students/altimin/calculator/CalculatorRunner.java
@@ -4,10 +4,11 @@ public class CalculatorRunner {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.err.println("Program should receive expression to calculate");
-            System.exit(0);
+            System.exit(1);
         }
         StringBuilder stringBuilder = new StringBuilder();
         for (String expressionPart : args) {
+            stringBuilder.append(" ");
             stringBuilder.append(expressionPart);
         }
         String expression = stringBuilder.toString();
@@ -16,6 +17,7 @@ public class CalculatorRunner {
             System.out.println(calculator.calculate());
         } catch (Exception e) {
             System.err.println(e.getMessage());
+            System.exit(1);
         }
     }
 }
