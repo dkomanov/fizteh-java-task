da892727412b04a178b4759a03b1a2436fee0f5b
Dmitriy Belyakov <dmitriy.b11@gmail.com>
2012-10-09 19:28:33 +0400

    Some changes in "wordCounter"

--- src/ru/fizteh/fivt/students/dmitriyBelyakov/wordCounter/WordCounter.java
+++ src/ru/fizteh/fivt/students/dmitriyBelyakov/wordCounter/WordCounter.java
@@ -25,13 +25,13 @@ public class WordCounter {
                     if (c == 'w') {
                         if (mode != Mode.NONE) {
                             System.err.println("Error: conflict of flags.");
-                            System.exit(-1);
+                            System.exit(1);
                         }
                         mode = Mode.MINUS_W;
                     } else if (c == 'l') {
                         if (mode != Mode.NONE) {
                             System.err.println("Error: conflict of flags.");
-                            System.exit(-1);
+                            System.exit(1);
                         }
                         mode = Mode.MINUS_L;
                     } else if (c == 'u') {
@@ -57,16 +57,16 @@ public class WordCounter {
             }
             System.out.print(out);
             if (!errorList.isEmpty()) {
-                System.exit(-1);
+                System.exit(1);
             }
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
-            System.exit(-1);
+            System.exit(1);
         }
     }
 
     static String makeOutInformation(String fileName, BigInteger i) {
-        String newLine = String.format("%n");
+        String newLine = System.getProperty("line.separator");
         StringBuilder builder = new StringBuilder();
         builder.append(fileName);
         builder.append(":");
@@ -77,7 +77,7 @@ public class WordCounter {
     }
 
     static String makeOutInformation(String fileName, HashMap<String, BigInteger> map) {
-        String newLine = String.format("%n");
+        String newLine = System.getProperty("line.separator");
         StringBuilder builder = new StringBuilder();
         builder.append(fileName);
         builder.append(":");
@@ -92,12 +92,12 @@ public class WordCounter {
     }
 
     static String makeOutInformation(BigInteger i) {
-        String newLine = String.format("%n");
+        String newLine = System.getProperty("line.separator");
         return i.toString() + newLine;
     }
 
     static String makeOutInformation(HashMap<String, BigInteger> map) {
-        String newLine = String.format("%n");
+        String newLine = System.getProperty("line.separator");
         StringBuilder builder = new StringBuilder();
         for (String key : map.keySet()) {
             builder.append(key);
@@ -109,14 +109,15 @@ public class WordCounter {
     }
 
     static String makeCalculations(ArrayList<String> fileNames, Mode mode, ModeUnique unique, boolean minusA, ArrayList<String> errorList) throws Exception {
-        BigInteger count = new BigInteger("0");
+        BigInteger count = BigInteger.ZERO;
         HashMap<String, BigInteger> map = new HashMap<String, BigInteger>();
         StringBuilder builder = new StringBuilder();
         for (String fileName : fileNames) {
             FileReader reader = null;
+            BufferedReader bufReader = null;
             try {
                 reader = new FileReader(fileName);
-                BufferedReader bufReader = new BufferedReader(reader);
+                bufReader = new BufferedReader(reader);
                 String str;
                 while ((str = bufReader.readLine()) != null) {
                     if (mode == Mode.MINUS_W) {
@@ -127,34 +128,36 @@ public class WordCounter {
                         } else if (unique == ModeUnique.MINUS_U) {
                             for (String word : words) {
                                 if (map.containsKey(word)) {
-                                    map.put(word, map.get(word).add(new BigInteger("1")));
+                                    map.put(word, map.get(word).add(BigInteger.ONE));
                                 } else {
-                                    map.put(word, new BigInteger("1"));
+                                    map.put(word, BigInteger.ONE);
                                 }
                             }
                         } else {
                             for (String word : words) {
-                                if (map.containsKey(word.toLowerCase())) {
-                                    map.put(word.toLowerCase(), map.get(word.toLowerCase()).add(new BigInteger("1")));
+                                String lWord = word.toLowerCase();
+                                if (map.containsKey(lWord)) {
+                                    map.put(lWord, map.get(lWord).add(BigInteger.ONE));
                                 } else {
-                                    map.put(word.toLowerCase(), new BigInteger("1"));
+                                    map.put(lWord, BigInteger.ONE);
                                 }
                             }
                         }
                     } else {
                         if (unique == ModeUnique.NONE) {
-                            count = count.add(new BigInteger("1"));
+                            count = count.add(BigInteger.ONE);
                         } else if (unique == ModeUnique.MINUS_U) {
                             if (map.containsKey(str)) {
-                                map.put(str, map.get(str).add(new BigInteger("1")));
+                                map.put(str, map.get(str).add(BigInteger.ONE));
                             } else {
-                                map.put(str, new BigInteger("1"));
+                                map.put(str, BigInteger.ONE);
                             }
                         } else {
-                            if (map.containsKey(str.toLowerCase())) {
-                                map.put(str.toLowerCase(), map.get(str.toLowerCase()).add(new BigInteger("1")));
+                            String lStr = str.toLowerCase();
+                            if (map.containsKey(lStr)) {
+                                map.put(lStr, map.get(lStr).add(BigInteger.ONE));
                             } else {
-                                map.put(str.toLowerCase(), new BigInteger("1"));
+                                map.put(lStr, BigInteger.ONE);
                             }
                         }
                     }
@@ -166,7 +169,7 @@ public class WordCounter {
                         builder.append(makeOutInformation(fileName, map));
                     }
                     map.clear();
-                    count = new BigInteger("0");
+                    count = BigInteger.ZERO;
                 }
             } catch (Exception e) {
                 if (minusA) {
@@ -176,6 +179,9 @@ public class WordCounter {
             } finally {
                 if (reader != null) {
                     reader.close();
+                }
+                if(bufReader != null) {
+                    bufReader.close();
                 }
             }
         }
