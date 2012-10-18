e79fd6d65e4e0c088f3ebfe63980640b5414bc62
Yulia Nikonova <nikonova.julya@gmail.com>
2012-10-18 13:15:07 +0400

    adding shell: final version

--- src/ru/fizteh/fivt/students/yuliaNikonova/shell/ArgumentParser.java
+++ src/ru/fizteh/fivt/students/yuliaNikonova/shell/ArgumentParser.java
@@ -3,18 +3,14 @@ package ru.fizteh.fivt.students.yuliaNikonova.shell;
 public class ArgumentParser {
 	private String[] mArgs;
 	public ArgumentParser (String[] args) {
-		for (int i = 0; i < args.length; i++) {
-			mArgs[i]=args[i];
-		}
-		
+		mArgs = args;
 	}
 	
 	public String parse() {
 		StringBuilder myCommandBuilder = new StringBuilder("");
 		for (String argument:mArgs) {
-			myCommandBuilder.append(argument);
+			myCommandBuilder.append(argument+" ");
 		}
-		
 		return myCommandBuilder.toString();
 	}
 
