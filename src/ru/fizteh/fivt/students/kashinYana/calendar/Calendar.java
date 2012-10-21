c9649d80187959cf1657cb94cf990ab62fd3f28a
Kashinskaya Yana <kashin.yana@gmail.com>
2012-10-21 18:00:59 +0400

    make more func add excep

--- src/ru/fizteh/fivt/students/kashinYana/calendar/Calendar.java
+++ src/ru/fizteh/fivt/students/kashinYana/calendar/Calendar.java
@@ -1,108 +1,153 @@
 package ru.fizteh.fivt.students.kashinYana.calendar;
 
-import javax.xml.crypto.Data;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 /**
- * Created with IntelliJ IDEA.
- * User: yana
- * Date: 20.10.12
- * Time: 23:09
- * To change this template use File | Settings | File Templates.
+ * User: Yana Kashinskaya, 195 group.
  */
 
 public class Calendar {
+
     static boolean isw = false;
     static boolean ist = false;
     static int numberMonth = -1;
     static String timeZone = "";
     static int numberYear = -1;
+
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < args.length; i++) {
-            if(args[i].equals("-m")) {
-                numberMonth = Integer.parseInt(args[i+1]);
+
+        try {
+            readKeys(args);
+        } catch (Exception e) {
+            System.err.println("Error in set keys");
+            System.exit(1);
+        }
+
+        GregorianCalendar calendar = new GregorianCalendar();
+        try {
+            setFlags(calendar);
+        } catch (Exception e) {
+            System.err.println("Incorrect value keys");
+            System.exit(1);
+        }
+        try {
+            printCalendar(calendar);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private static void readKeys(String[] args) throws Exception {
+        int i = 0;
+        while (i < args.length) {
+            if (args[i].equals("-m")) {
+                numberMonth = Integer.parseInt(args[i + 1]);
+                i += 2;
             } else if (args[i].equals("-w")) {
                 isw = true;
+                i++;
             } else if (args[i].endsWith("-t")) {
                 ist = true;
                 timeZone = args[i + 1];
+                i += 2;
             } else if (args[i].equals("-y")) {
-                numberYear = Integer.parseInt(args[i+1]);
+                numberYear = Integer.parseInt(args[i + 1]);
+                i += 2;
+            } else {
+                throw new Exception("Unknown key.");
             }
         }
         if (timeZone.equals("")) {
             timeZone = TimeZone.getDefault().getID();
         }
-        GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone(timeZone));
+    }
+
+    private static void setFlags(GregorianCalendar calendar) throws Exception {
+        calendar.setTimeZone(TimeZone.getTimeZone(timeZone));
         Date curData = new Date();
         if (numberMonth == -1) {
-            SimpleDateFormat ft = new SimpleDateFormat ("M");
+            SimpleDateFormat ft = new SimpleDateFormat("M");
             numberMonth = Integer.parseInt(ft.format(curData));
         }
         if (numberYear == -1) {
-            SimpleDateFormat ft = new SimpleDateFormat ("yyyy");
+            SimpleDateFormat ft = new SimpleDateFormat("yyyy");
             numberYear = Integer.parseInt(ft.format(curData));
         }
+        if (numberMonth < 1 || numberMonth > 12 || numberYear > 9999 || numberYear < 1000) {
+            throw new Exception("Incorrect value key.");
+        }
         calendar.set(numberYear, numberMonth - 1, 1);
-        if(isw) {
+    }
+
+    private static void printCalendar(GregorianCalendar calendar) throws Exception {
+        //печать заголовка
+        if (isw) {
             System.out.printf("   ");
         }
         System.out.printf("   %tB %<tY\n", calendar.getTime());
-        int idweek =  calendar.get(GregorianCalendar.WEEK_OF_YEAR);
-        if(isw) {
+        if (isw) {
             System.out.printf("   ");
         }
         System.out.println("Mo Tu We Th Fr Sa Su");
-        int id = 0;
-        if(isw) {
-            System.out.printf("%2d ", idweek);
+
+        //печать начала первой недели
+        int idWeek = calendar.get(GregorianCalendar.WEEK_OF_YEAR);
+        if (isw) {
+            System.out.printf("%2d ", idWeek);
         }
-        for (id = 0; id < firstDay(calendar); id++) {
+        int dayWeek = 0;
+        for (dayWeek = 0; dayWeek < firstDay(calendar); dayWeek++) {
             System.out.print("   ");
         }
-        int numberDays = calendar.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-        for (int i =  1; i <= numberDays; i++) {
-            System.out.printf("%2d ",i);
-            id++;
-            if(id == 7) {
-                id = 0;
-                idweek++;
-                System.out.print("\n");
-                if(isw && i < numberDays) {
-                    System.out.printf("%2d ", idweek);
-                }
 
+        //печать всего месяца
+        int numberDays = calendar.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+        for (int dayMonth = 1; dayMonth <= numberDays; dayMonth++) {
+            System.out.printf("%2d ", dayMonth);
+            dayWeek++;
+            if (dayWeek == 7) {
+                dayWeek = 0;
+                idWeek++;
+                System.out.print("\n");
+                if (isw && dayMonth < numberDays) {
+                    System.out.printf("%2d ", idWeek);
+                }
             }
         }
         System.out.println();
-        System.out.println();
-        if(isw) {
+
+        //печать текущей даты, если нужно
+        if (isw) {
+            System.out.println();
             System.out.printf("Now: %tY.%<tm.%<td %<tT ", calendar.getTime());
-            System.out.println(calendar.getTimeZone().getID() + " Time");
+            int idSlesh = timeZone.lastIndexOf("/");
+            System.out.println(timeZone.substring(idSlesh + 1) + " Time");
         }
     }
 
-    private static int firstDay(GregorianCalendar calendar) {
-        SimpleDateFormat ft = new SimpleDateFormat ("E");
+    private static int firstDay(GregorianCalendar calendar) throws Exception {
+        SimpleDateFormat ft = new SimpleDateFormat("E");
         String weekday = ft.format(calendar.getTime());
         if (weekday.equals("Mon")) {
             return 0;
-        } else if(weekday.equals("Tue")) {
+        } else if (weekday.equals("Tue")) {
             return 1;
         } else if (weekday.equals("Wed")) {
             return 2;
         } else if (weekday.equals("Thu")) {
             return 3;
-        }  else if (weekday.equals("Fri")) {
+        } else if (weekday.equals("Fri")) {
             return 4;
-        }  else if (weekday.equals("Sat")) {
+        } else if (weekday.equals("Sat")) {
             return 5;
-        }  else if (weekday.equals("Sun")){
+        } else if (weekday.equals("Sun")) {
             return 6;
+        } else {
+            throw new Exception("Unknown month");
         }
-        return 7;
     }
 }
