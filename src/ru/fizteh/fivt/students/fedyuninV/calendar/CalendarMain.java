1384ba69cf4d6b25d0b19ebe7701f44018951902
FedyuninV <stelz40494@gmail.com>
2012-10-24 14:19:35 +0400

    unused import removed

--- src/ru/fizteh/fivt/students/fedyuninV/calendar/CalendarMain.java
+++ src/ru/fizteh/fivt/students/fedyuninV/calendar/CalendarMain.java
@@ -5,7 +5,6 @@ import java.text.DateFormat;
 import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 import java.util.TimeZone;
 import java.util.Calendar;
 
