b765612959446f75b5e17d95da3b648fec46c77f
kogemrka <kogemrka@gmail.com>
2012-10-29 19:50:06 +0400

    Вынужденно отрекаюсь от церкви любителей табов и примыкаю к секте четырёх пробелов.

--- src/ru/fizteh/fivt/students/nikitaAntonov/calculator/CalculatorException.java
+++ src/ru/fizteh/fivt/students/nikitaAntonov/calculator/CalculatorException.java
@@ -8,12 +8,12 @@ package ru.fizteh.fivt.students.nikitaAntonov.calculator;
 @SuppressWarnings("serial")
 public class CalculatorException extends Exception {
 
-	/* Вопрос: почему нельзя просто взять и отнаследоваться от Exception?
-	 * PS. Это плохо, оставлять такие вопросы в коде? Мб лучше задавать
-	 * их в комментариях к комиту?
-	 */
-	
-	public  CalculatorException(String message) {
+    /* Вопрос: почему нельзя просто взять и отнаследоваться от Exception?
+     * PS. Это плохо, оставлять такие вопросы в коде? Мб лучше задавать
+     * их в комментариях к комиту?
+     */
+
+    public  CalculatorException(String message) {
         super(message);
     }
 }
--- src/ru/fizteh/fivt/students/nikitaAntonov/calculator/Lexem.java
+++ src/ru/fizteh/fivt/students/nikitaAntonov/calculator/Lexem.java
@@ -6,61 +6,61 @@ package ru.fizteh.fivt.students.nikitaAntonov.calculator;
  * @author Антонов Никита
  */
 class Lexem {
-	/* XXX Очень не хватает union`ов из C/C++
-	 * для реализации классов такого рода, приходится делать некрасиво,
-	 * дабы не городить иерархию классов и приведение типов на каждом шагу.
-	 */
-	
-	public enum Type {
-		BRACKET_OPEN, BRACKET_CLOSE, OP_LEVEL1, OP_LEVEL2, NUM, END, BEGIN
-	}
-	
-	public enum Op1Type	{
-		PLUS, MINUS
-	}
-	
-	public enum Op2Type {
-		MULT, DIV, MOD
-	}
-		
-	public Type type;
-	public int number;
-	public Op1Type type1;
-	public Op2Type type2;
-	
-	public Lexem(int num) {
-		type = Type.NUM;
-		number = num;
-	}
-	
-	public Lexem(Type t) {
-		type = t;
-	}
-	
-	public Lexem(char c) throws CalculatorException {
-		switch (c) {
-		case '+':
-			type = Type.OP_LEVEL1;
-			type1 = Op1Type.PLUS;
-			break;
-		case '-':
-			type = Type.OP_LEVEL1;
-			type1 = Op1Type.MINUS;
-			break;
-		case '*':
-			type = Type.OP_LEVEL2;
-			type2 = Op2Type.MULT;
-			break;
-		case '/':
-			type = Type.OP_LEVEL2;
-			type2 = Op2Type.DIV;
-			break;
-		case '%':
-			type = Type.OP_LEVEL2;
-			type2 = Op2Type.MOD;
-			break;
-		default:
-			throw new CalculatorException("Incorrect symbol of operation: " + c);
-		}
-	}
+    /* XXX Очень не хватает union`ов из C/C++
+     * для реализации классов такого рода, приходится делать некрасиво,
+     * дабы не городить иерархию классов и приведение типов на каждом шагу.
+     */
+
+    public enum Type {
+        BRACKET_OPEN, BRACKET_CLOSE, OP_LEVEL1, OP_LEVEL2, NUM, END, BEGIN
+    }
+
+    public enum Op1Type {
+        PLUS, MINUS
+    }
+
+    public enum Op2Type {
+        MULT, DIV, MOD
+    }
+
+    public Type type;
+    public int number;
+    public Op1Type type1;
+    public Op2Type type2;
+
+    public Lexem(int num) {
+        type = Type.NUM;
+        number = num;
+    }
+
+    public Lexem(Type t) {
+        type = t;
+    }
+
+    public Lexem(char c) throws CalculatorException {
+        switch (c) {
+        case '+':
+            type = Type.OP_LEVEL1;
+            type1 = Op1Type.PLUS;
+            break;
+        case '-':
+            type = Type.OP_LEVEL1;
+            type1 = Op1Type.MINUS;
+            break;
+        case '*':
+            type = Type.OP_LEVEL2;
+            type2 = Op2Type.MULT;
+            break;
+        case '/':
+            type = Type.OP_LEVEL2;
+            type2 = Op2Type.DIV;
+            break;
+        case '%':
+            type = Type.OP_LEVEL2;
+            type2 = Op2Type.MOD;
+            break;
+        default:
+            throw new CalculatorException("Incorrect symbol of operation: " + c);
+        }
+    }
 }
