e6c11f641361f96976752a93c34ad616b523f878
kogemrka <kogemrka@gmail.com>
2012-10-29 15:58:09 +0400

    Добавлен старый недоделанный код

---
+++ src/ru/fizteh/fivt/students/nikitaAntonov/wordcounter/WordCounter.java
@@ -0,0 +1,16 @@
+/*
+ * Антонов Никита kogemrka@gmail.com
+ * ФИВТ, 197 группа 
+ */
+package ru.fizteh.fivt.kogemrka;
+
+public class WordCounter {
+	
+	static enum Options {
+		
+	}
+	
+	public static void main(String args[]) {
+		
+	}
+}
