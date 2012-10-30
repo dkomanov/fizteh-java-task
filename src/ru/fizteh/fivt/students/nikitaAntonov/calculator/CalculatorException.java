61d17065fd448882c40aa82ab1f0f427ef37b9cb
kogemrka <kogemrka@gmail.com>
2012-10-30 10:49:10 +0400

    Убрано подавление warning`а в классе-исключении

--- src/ru/fizteh/fivt/students/nikitaAntonov/calculator/CalculatorException.java
+++ src/ru/fizteh/fivt/students/nikitaAntonov/calculator/CalculatorException.java
@@ -5,13 +5,9 @@ package ru.fizteh.fivt.students.nikitaAntonov.calculator;
  * 
  * @author Антонов Никита
  */
-@SuppressWarnings("serial")
 public class CalculatorException extends Exception {
 
-    /* Вопрос: почему нельзя просто взять и отнаследоваться от Exception?
-     * PS. Это плохо, оставлять такие вопросы в коде? Мб лучше задавать
-     * их в комментариях к комиту?
-     */
+    private static final long serialVersionUID = -5153070149080169560L;
 
     public  CalculatorException(String message) {
         super(message);
