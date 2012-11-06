0441e95eaaab96716642e34260efc8fe06b7dd96
Pavlov Konstantin <pavlov.konstantin@phystech.edu>
2012-11-07 02:04:08 +0400

    Task 02 fixed 1

--- src/ru/fizteh/fivt/students/konstantinPavlov/wordCounter/WordCounter.java
+++ src/ru/fizteh/fivt/students/konstantinPavlov/wordCounter/WordCounter.java
@@ -10,216 +10,254 @@ import java.util.Map;
 
 public class WordCounter {
 
-	static int settingL = 0;
-	static int settingW = 0;
-	static int settingNoRegister = 0;
-	static int settingRegister = 0;
-	static int settingA = 0;
+    static boolean settingL = false;
+    static boolean settingW = false;
+    static boolean settingNoRegister = false;
+    static boolean settingRegister = false;
+    static boolean settingA = false;
 
-	static int globalLinesAmount = 0;
-	static int globalWordsAmount = 0;
-	static Map<String, Integer> globalMap = new HashMap<String, Integer>();
-	private static BufferedReader bufferedReader;
+    static int globalLinesAmount = 0;
+    static int globalWordsAmount = 0;
 
-	static void analyzeFile(String path) throws IOException {
+    static Map<String, Integer> globalMap = new HashMap<String, Integer>();
+    static Map<String, String> globalLowerCaseMap = new HashMap<String, String>();
 
-		File file = new File(path);
-		if (!file.canRead()) {
-			System.err.println("Error: file \"" + file.getPath()
-					+ "\" can't be opened or readed");
-			System.exit(1);
-		}
-		FileInputStream fis = new FileInputStream(file);
-		InputStreamReader streamReader = new InputStreamReader(fis);
-		bufferedReader = new BufferedReader(streamReader);
+    private static BufferedReader bufferedReader;
 
-		String str = new String();
-		int localLinesAmount = 0;
-		int localWordsAmount = 0;
-		Map<String, Integer> localMap = new HashMap<String, Integer>();
+    static void analyzeFile(String path) throws IOException {
+        FileInputStream fis = null;
+        InputStreamReader streamReader = null;
 
-		// start analyzing
-		while ((str = bufferedReader.readLine()) != null) {
-			// toLowerCase if register is not important
-			if (settingNoRegister == 1) {
-				str = str.toLowerCase();
-			}
+        try {
+            File file = new File(path);
+            if (!file.canRead()) {
+                System.err.println("Error: file \"" + file.getPath()
+                        + "\" can't be opened or readed");
+                System.exit(1);
+            }
+            fis = new FileInputStream(file);
+            streamReader = new InputStreamReader(fis);
+            bufferedReader = new BufferedReader(streamReader);
 
-			if (settingW == 1) {
-				// count words
+            String str;
+            int localLinesAmount = 0;
+            int localWordsAmount = 0;
+            Map<String, Integer> localMap = new HashMap<String, Integer>();
+            Map<String, String> localLowerCaseMap = new HashMap<String, String>();
 
-				// parsing the string to words
-				String[] arrayOfWords = str.split(" ");
+            // start analyzing
+            String searchString;
+            while ((str = bufferedReader.readLine()) != null) {
 
-				for (int i = 0; i < arrayOfWords.length; ++i) {
-					// add word to our map
-					if (settingA != 1) {
-						if (localMap.containsKey(arrayOfWords[i])
-								&& !arrayOfWords[i].equals(" ")
-								&& !arrayOfWords[i].equals("\t")) {
-							++localWordsAmount;
-							localMap.put(arrayOfWords[i],
-									localMap.get(arrayOfWords[i]) + 1);
-						} else {
-							if (!arrayOfWords[i].equals(" ")
-									&& !arrayOfWords[i].equals("\t")) {
-								++localWordsAmount;
-								localMap.put(arrayOfWords[i], 1);
-							}
-						}
-					} else {
-						if (globalMap.containsKey(arrayOfWords[i])
-								&& !arrayOfWords[i].equals(" ")
-								&& !arrayOfWords[i].equals("\t")) {
-							++globalWordsAmount;
-							globalMap.put(arrayOfWords[i],
-									globalMap.get(arrayOfWords[i]) + 1);
-						} else {
-							if (!arrayOfWords[i].equals(" ")
-									&& !arrayOfWords[i].equals("\t")) {
-								++globalWordsAmount;
-								globalMap.put(arrayOfWords[i], 1);
-							}
-						}
-					}
-				}
-			} else {
-				// count lines
-				if (settingNoRegister != 1 && settingRegister != 1) {
-					if (settingA != 1) {
-						++localLinesAmount;
-					} else {
-						++globalLinesAmount;
-					}
-				} else {
-					// count lines without register
-					if (settingA != 1) {
-						if (localMap.containsKey(str) && !str.equals("")) {
-							localMap.put(str, localMap.get(str) + 1);
-						} else {
-							if (!str.equals("")) {
-								localMap.put(str, 1);
-							}
-						}
-					} else {
-						if (globalMap.containsKey(str) && !str.equals("")) {
-							globalMap.put(str, globalMap.get(str) + 1);
-						} else {
-							if (!str.equals("")) {
-								globalMap.put(str, 1);
-							}
-						}
-					}
-				}
+                if (settingW) {
+                    // count words
 
-			}
+                    // parsing the string to words
+                    String[] arrayOfWords = str
+                            .split("[\\s\\.:;,\\\"\\\'\\(\\)!]+");
 
-		}
-		// finish analyzing
+                    for (int i = 0; i < arrayOfWords.length; ++i) {
+                        // add word to our map
+                        if (settingNoRegister) {
+                            searchString = arrayOfWords[i].toLowerCase();
+                        } else {
+                            searchString = arrayOfWords[i];
+                        }
 
-		// show information about analysis
-		if (settingA != 1) {
-			System.out.println(file.getPath() + ":");
-			if ((settingW == 1 || settingL == 1)
-					&& (settingRegister == 1 || settingNoRegister == 1)) {
-				for (Map.Entry<String, Integer> entry : localMap.entrySet()) {
-					System.out.println(entry.getKey() + " " + entry.getValue());
-				}
-			} else {
-				if (settingW == 1) {
-					System.out.println(localWordsAmount);
-				} else {
-					System.out.println(localLinesAmount);
-				}
-			}
-		}
+                        if (!settingA) {
+                            if (localMap.containsKey(searchString)
+                                    && !searchString.equals(" ")
+                                    && !searchString.equals("\t")) {
+                                ++localWordsAmount;
+                                localMap.put(searchString,
+                                        localMap.get(searchString) + 1);
+                            } else {
+                                if (!searchString.equals(" ")
+                                        && !searchString.equals("\t")) {
+                                    ++localWordsAmount;
+                                    localMap.put(searchString, 1);
+                                    localLowerCaseMap.put(searchString,
+                                            arrayOfWords[i]);
+                                }
+                            }
+                        } else {
+                            if (globalMap.containsKey(searchString)
+                                    && !searchString.equals(" ")
+                                    && !searchString.equals("\t")) {
+                                ++globalWordsAmount;
+                                globalMap.put(searchString,
+                                        globalMap.get(searchString) + 1);
+                            } else {
+                                if (!searchString.equals(" ")
+                                        && !searchString.equals("\t")) {
+                                    ++globalWordsAmount;
+                                    globalMap.put(searchString, 1);
+                                    globalLowerCaseMap.put(searchString,
+                                            arrayOfWords[i]);
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    // count lines
+                    if (settingNoRegister) {
+                        searchString = str.toLowerCase();
+                    } else {
+                        searchString = str;
+                    }
 
-	}
+                    if (!settingNoRegister && !settingRegister) {
+                        if (!settingA) {
+                            ++localLinesAmount;
+                        } else {
+                            ++globalLinesAmount;
+                        }
+                    } else {
+                        // count lines without register
+                        if (!settingA) {
+                            if (localMap.containsKey(searchString)
+                                    && !searchString.isEmpty()) {
+                                localMap.put(searchString,
+                                        localMap.get(searchString) + 1);
+                            } else {
+                                if (!searchString.isEmpty()) {
+                                    localMap.put(searchString, 1);
+                                    localLowerCaseMap.put(searchString, str);
+                                }
+                            }
+                        } else {
+                            if (globalMap.containsKey(searchString)
+                                    && !searchString.isEmpty()) {
+                                globalMap.put(searchString,
+                                        globalMap.get(searchString) + 1);
+                            } else {
+                                if (!searchString.isEmpty()) {
+                                    globalMap.put(searchString, 1);
+                                    localLowerCaseMap.put(searchString, str);
+                                }
+                            }
+                        }
+                    }
 
-	static void showAnalysisA() {
-		if (settingA == 1) {
-			if ((settingW == 1 || settingL == 1)
-					&& (settingRegister == 1 || settingNoRegister == 1)) {
-				for (Map.Entry<String, Integer> entry : globalMap.entrySet()) {
-					System.out.println(entry.getKey() + " " + entry.getValue());
-				}
-			} else {
-				if (settingW == 1) {
-					System.out.println(globalWordsAmount);
-				} else {
-					System.out.println(globalLinesAmount);
-				}
-			}
-		}
-	}
+                }
 
-	public static void setSettings(String[] args) {
-		for (String str : args) {
-			if (str.isEmpty() || str.charAt(0) != '-') {
-				continue;
-			} else {
-				for (int i = 1; i < str.length(); ++i) {
-					switch (str.charAt(i)) {
-					case 'l':
-						if (settingW == 1) {
-							System.err.println("Error: flag w has already set");
-							System.exit(1);
-						}
-						settingL = 1;
-						break;
+            }
+            // finish analyzing
 
-					case 'w':
-						if (settingL == 1) {
-							System.err.println("Error: flag l has already set");
-							System.exit(1);
-						}
-						settingW = 1;
-						break;
+            // show information about analysis
+            if (!settingA) {
+                System.out.println(file.getPath() + ":");
+                if ((settingW || settingL)
+                        && (settingRegister || settingNoRegister)) {
+                    for (Map.Entry<String, Integer> entry : localMap.entrySet()) {
+                        System.out
+                                .println(localLowerCaseMap.get(entry.getKey())
+                                        + " " + entry.getValue());
+                    }
+                } else {
+                    if (settingW) {
+                        System.out.println(localWordsAmount);
+                    } else {
+                        System.out.println(localLinesAmount);
+                    }
+                }
+            }
+        } catch (Exception expt) {
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (Exception expt) {
+                }
+            }
 
-					case 'u':
-						if (settingNoRegister == 1) {
-							System.err.println("Error: flag U has alredy set");
-							System.exit(1);
-						}
-						settingRegister = 1;
-						break;
+            if (streamReader != null) {
+                try {
+                    streamReader.close();
+                } catch (Exception expt) {
+                }
+            }
+        }
 
-					case 'U':
-						if (settingRegister == 1) {
-							System.err.println("Error: flag u has alredy set");
-							System.exit(1);
-						}
-						settingNoRegister = 1;
-						break;
+    }
 
-					case 'a':
-						settingA = 1;
-						break;
+    static void showAnalysisA() {
+        if (settingA) {
+            if (settingW || settingL) {
+                for (Map.Entry<String, Integer> entry : globalMap.entrySet()) {
+                    System.out.println(globalLowerCaseMap.get(entry.getKey())
+                            + " " + entry.getValue());
+                }
+            }
+        }
+    }
 
-					default:
-						System.err.println("Error: wrong flag: "
-								+ str.charAt(i));
-						System.exit(1);
-						break;
-					}
-				}
-			}
-		}
-		if(settingL==0 && settingW==0) settingW=0;
-	}
+    public static void setSettings(String[] args) {
+        for (String str : args) {
+            if (str.isEmpty() || str.charAt(0) != '-') {
+                continue;
+            } else {
+                for (int i = 1; i < str.length(); ++i) {
+                    switch (str.charAt(i)) {
+                    case 'l':
+                        if (settingW) {
+                            System.err.println("Error: flag w has already set");
+                            System.exit(1);
+                        }
+                        settingL = true;
+                        break;
 
-	public static void main(String[] args) throws IOException {
-		String path = new String();
+                    case 'w':
+                        if (settingL) {
+                            System.err.println("Error: flag l has already set");
+                            System.exit(1);
+                        }
+                        settingW = true;
+                        break;
 
-		setSettings(args);
+                    case 'u':
+                        if (settingNoRegister) {
+                            System.err.println("Error: flag U has alredy set");
+                            System.exit(1);
+                        }
+                        settingRegister = true;
+                        break;
 
-		for (int i = 0; i < args.length; ++i) {
-			path = args[i];
-			if (path.isEmpty() || path.charAt(0) == '-') {
-				continue;
-			}
-			analyzeFile(path);
-		}
-		showAnalysisA();
-	}
+                    case 'U':
+                        if (settingRegister) {
+                            System.err.println("Error: flag u has alredy set");
+                            System.exit(1);
+                        }
+                        settingNoRegister = true;
+                        break;
+
+                    case 'a':
+                        settingA = true;
+                        break;
+
+                    default:
+                        System.err.println("Error: wrong flag: "
+                                + str.charAt(i));
+                        System.exit(1);
+                        break;
+                    }
+                }
+            }
+        }
+        if (!settingL && !settingW)
+            settingW = true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        setSettings(args);
+
+        for (int i = 0; i < args.length; ++i) {
+            String path = args[i];
+            if (path.isEmpty() || path.charAt(0) == '-') {
+                continue;
+            }
+            analyzeFile(path);
+        }
+        showAnalysisA();
+    }
 }
