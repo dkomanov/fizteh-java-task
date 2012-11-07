4ff5b31781f500aeffc9670ae525ac93f6b5d323
Alexander Kuzmin <kyzmin.alexandr@phystech.edu>
2012-11-08 03:01:25 +0400

    WordCounter
    Version 1.1

--- src/ru/fizteh/fivt/students/alexanderKuzmin/wordCounter/WordCounter.java
+++ src/ru/fizteh/fivt/students/alexanderKuzmin/wordCounter/WordCounter.java
@@ -1,6 +1,7 @@
 package ru.fizteh.fivt.students.alexanderKuzmin.wordCounter;
 
 import java.io.BufferedReader;
+import java.io.Closeable;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -26,20 +27,14 @@ public class WordCounter {
         System.exit(1);
     }
 
-    private static void closeAllReaders(FileReader freader,
-            BufferedReader breader) {
-        if (freader != null)
+    private static <T extends Closeable> void closeStream(T stream) {
+        if (stream != null) {
             try {
-                freader.close();
+                stream.close();
             } catch (IOException e) {
-                printErrAndExit(e.getClass().getName());
+                printErrAndExit(e.getClass().getName().toString());
             }
-        if (breader != null)
-            try {
-                breader.close();
-            } catch (IOException e) {
-                printErrAndExit(e.getClass().getName());
-            }
+        }
     }
 
     void worker(String[] name, int n, Mode mod) {
@@ -53,13 +48,13 @@ public class WordCounter {
             Integer count = 0;
             HashMap<String, Integer> hmap = new HashMap<String, Integer>();
             for (int i = n; i < name.length; ++i) {
-                FileReader freader = null;
-                BufferedReader breader = null;
+                FileReader fReader = null;
+                BufferedReader bReader = null;
                 try {
-                    freader = new FileReader(name[i]);
-                    breader = new BufferedReader(freader);
+                    fReader = new FileReader(name[i]);
+                    bReader = new BufferedReader(fReader);
                     String curline;
-                    while ((curline = breader.readLine()) != null) {
+                    while ((curline = bReader.readLine()) != null) {
                         if (mod.l) {
                             ++count;
                             if (mod.U || mod.u) {
@@ -91,9 +86,10 @@ public class WordCounter {
                         }
                     }
                 } catch (Exception e) {
-                    printErrAndExit(e.getClass().getName());
+                    printErrAndExit(e.getClass().getName().toString());
                 } finally {
-                    closeAllReaders(freader, breader);
+                    closeStream(fReader);
+                    closeStream(bReader);
                 }
                 if (!mod.a) {
                     System.out.println(name[i] + ":");
