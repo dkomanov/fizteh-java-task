a3915995eb07ec261c4b68673c409869281677e4
= <tolya@tolya-.(none)>
2012-11-08 19:01:24 +0300

    fixed enum and added lowercase comparator

--- src/ru/fizteh/fivt/students/tolyapro/wordCounter/Counter.java
+++ src/ru/fizteh/fivt/students/tolyapro/wordCounter/Counter.java
@@ -5,25 +5,26 @@ import java.io.*;
 import java.util.*;
 
 enum Mode {
-    Err, None, Lu, LU, Wu, WU, L, W
+    ERR, NONE, LINESCASENONSENSITIVE, LINESCASESENSITIVE, WORDSCASENONSENSITIVE, WORDSCASESENSITIVE, LINES, WORDS
 }
 
 public class Counter {
 
     public static boolean modeWords(Mode mode) {
-        return ((mode == Mode.Wu) || (mode == Mode.WU) || (mode == Mode.W));
+        return ((mode == Mode.WORDS) || (mode == Mode.WORDSCASESENSITIVE) || (mode == Mode.WORDSCASENONSENSITIVE));
     }
 
     public static boolean modeLines(Mode mode) {
-        return ((mode == Mode.Lu) || (mode == Mode.LU) || (mode == Mode.L));
+        return ((mode == Mode.LINESCASENONSENSITIVE)
+                || (mode == Mode.LINESCASESENSITIVE) || (mode == Mode.LINES));
     }
 
     public static boolean modeUniqueWithReg(Mode mode) {
-        return ((mode == Mode.WU) || (mode == Mode.LU));
+        return ((mode == Mode.WORDSCASESENSITIVE) || (mode == Mode.LINESCASESENSITIVE));
     }
 
     public static boolean modeUnique(Mode mode) {
-        return ((mode == Mode.Wu) || (mode == Mode.Lu));
+        return ((mode == Mode.WORDSCASENONSENSITIVE) || (mode == Mode.LINESCASENONSENSITIVE));
     }
 
     public static boolean checkEmpty(String[] args) {
@@ -32,8 +33,7 @@ public class Counter {
 
     public static boolean modeAgregate = false;
 
-    public static void addWord(Map<String, Integer> dict,
-            Map<String, String> lowerCaseWords, String word, Mode mode) {
+    public static void addWord(Map<String, Integer> dict, String word, Mode mode) {
         if (modeUniqueWithReg(mode)) {
             Integer amount;
             amount = dict.get(word);
@@ -44,14 +44,14 @@ public class Counter {
             }
         } else {
             Integer amount;
-            String lowerCaseWord = word.toLowerCase();
-            if (!lowerCaseWords.containsKey(lowerCaseWord))
-                lowerCaseWords.put(lowerCaseWord, word);
-            amount = dict.get(lowerCaseWord);
-            if (dict.get(lowerCaseWord) != null) {
-                dict.put(lowerCaseWord, amount + 1);
+            // String lowerCaseWord = word.toLowerCase();
+            // if (!lowerCaseWords.containsKey(lowerCaseWord))
+            // lowerCaseWords.put(lowerCaseWord, word);
+            amount = dict.get(word);
+            if (dict.get(word) != null) {
+                dict.put(word, amount + 1);
             } else {
-                dict.put(lowerCaseWord, 1);
+                dict.put(word, 1);
             }
         }
 
@@ -59,46 +59,47 @@ public class Counter {
 
     public static Mode getParam(char c, Mode mode) {
         if (c == 'l') {
-            if (mode == Mode.None) {
-                mode = Mode.L;
+            if (mode == Mode.NONE) {
+                mode = Mode.LINES;
             } else {
-                return Mode.Err;
+                return Mode.ERR;
             }
 
         } else if (c == 'w') {
-            if (mode == Mode.None) {
-                mode = Mode.W;
+            if (mode == Mode.NONE) {
+                mode = Mode.WORDS;
             } else {
-                return Mode.Err;
+                return Mode.ERR;
             }
         } else if (c == 'u') {
-            if (mode == Mode.L) {
-                mode = Mode.Lu;
-            } else if (mode == Mode.W) {
-                mode = Mode.Wu;
+            if (mode == Mode.LINES) {
+                mode = Mode.LINESCASENONSENSITIVE;
+            } else if (mode == Mode.WORDS) {
+                mode = Mode.WORDSCASENONSENSITIVE;
             } else {
-                return Mode.Err;
+                return Mode.ERR;
             }
         } else if (c == 'U') {
-            if (mode == Mode.L) {
-                mode = Mode.LU;
-            } else if (mode == Mode.W) {
-                mode = Mode.WU;
+            if (mode == Mode.LINES) {
+                mode = Mode.LINESCASESENSITIVE;
+            } else if (mode == Mode.WORDS) {
+                mode = Mode.WORDSCASESENSITIVE;
             } else {
-                return Mode.Err;
+                return Mode.ERR;
             }
         } else if (c == 'a') {
             modeAgregate = true;
         } else {
-            mode = Mode.Err;
+            mode = Mode.ERR;
         }
         return mode;
     }
 
     public static void main(String[] args) throws Exception {
-        Mode mode = Mode.None;
+        Mode mode = Mode.NONE;
         if (checkEmpty(args)) {
             System.out.println("Usage: java Counter [keys] FILE1 FILE2 ...");
+            System.exit(1);
         }
         int paramNumb = 0;
         for (String s : args) {
@@ -120,15 +121,19 @@ public class Counter {
                 break;
             }
         }
-        if (mode == Mode.Err) {
+        if (mode == Mode.ERR) {
             System.out.println("Wrong parametres combination");
             System.exit(1);
         }
-        if ((paramNumb == 0) || (mode == Mode.None)) {
-            mode = Mode.W;
+        if ((paramNumb == 0) || (mode == Mode.NONE)) {
+            mode = Mode.WORDS;
         }
-        Map<String, Integer> dict = new HashMap<String, Integer>();
-        Map<String, String> lowerCaseWords = new HashMap<String, String>();
+        Map<String, Integer> dict;
+        if (modeUnique(mode) && !modeUniqueWithReg(mode)) {
+            dict = new TreeMap<String, Integer>(String.CASE_INSENSITIVE_ORDER);
+        } else {
+            dict = new TreeMap<String, Integer>();
+        }
         for (int i = paramNumb; i < args.length; ++i) {
             String s = args[i];
             DataInputStream in = null;
@@ -141,11 +146,11 @@ public class Counter {
                     if (modeWords(mode)) {
                         String[] words = strLine.split("[\\s|\t|:;,.()\\[\\]]");
                         for (String word : words) {
-                            addWord(dict, lowerCaseWords, word, mode);
+                            addWord(dict, word, mode);
                         }
                     } else {
                         if (modeLines(mode)) {
-                            addWord(dict, lowerCaseWords, strLine, mode);
+                            addWord(dict, strLine, mode);
                         }
                     }
                 }
@@ -159,23 +164,13 @@ public class Counter {
                 BufferCloser.close(br);
             }
             if (!modeAgregate) {
-                if (modeUnique(mode)) {
+                if (modeUnique(mode) || modeUniqueWithReg(mode)) {
                     System.out.println("File " + args[i] + " :");
-                    if (!modeUniqueWithReg(mode)) {
-                        Iterator<String> iterator = dict.keySet().iterator();
-                        while (iterator.hasNext()) {
-                            String word = iterator.next().toString();
-                            String lowerCaseWord = lowerCaseWords.get(word);
-                            String amount = dict.get(word).toString();
-                            System.out.println(lowerCaseWord + " " + amount);
-                        }
-                    } else {
-                        Iterator<String> iterator = dict.keySet().iterator();
-                        while (iterator.hasNext()) {
-                            String word = iterator.next().toString();
-                            String amount = dict.get(word).toString();
-                            System.out.println(word + " " + amount);
-                        }
+                    Iterator<String> iterator = dict.keySet().iterator();
+                    while (iterator.hasNext()) {
+                        String word = iterator.next().toString();
+                        String amount = dict.get(word).toString();
+                        System.out.println(word + " " + amount);
                     }
                 } else {
                     Iterator<String> iterator = dict.keySet().iterator();
@@ -188,7 +183,7 @@ public class Counter {
                     System.out.println(sum);
                 }
                 dict.clear();
-                lowerCaseWords.clear();
+
             }
         }
         if (modeAgregate) {
