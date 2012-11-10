099182549a674e77d06c67a6a879661646bc4cef
Yulia Nikonova <nikonova.julya@gmail.com>
2012-11-10 10:31:24 +0400

    tab in argParser

--- src/ru/fizteh/fivt/students/yuliaNikonova/shell/ArgumentParser.java
+++ src/ru/fizteh/fivt/students/yuliaNikonova/shell/ArgumentParser.java
@@ -1,18 +1,19 @@
 package ru.fizteh.fivt.students.yuliaNikonova.shell;
 
 public class ArgumentParser {
-	private String[] mArgs;
-	public ArgumentParser (String[] args) {
-		mArgs = args;
-	}
-	
-	public String parse() {
-		StringBuilder myCommandBuilder = new StringBuilder("");
-		for (String argument : mArgs) {
-			myCommandBuilder.append(argument);
-			myCommandBuilder.append(" ");
-		}
-		return myCommandBuilder.toString();
-	}
+    private String[] mArgs;
+
+    public ArgumentParser(String[] args) {
+        mArgs = args;
+    }
+
+    public String parse() {
+        StringBuilder myCommandBuilder = new StringBuilder("");
+        for (String argument : mArgs) {
+            myCommandBuilder.append(argument);
+            myCommandBuilder.append(" ");
+        }
+        return myCommandBuilder.toString();
+    }
 
 }
