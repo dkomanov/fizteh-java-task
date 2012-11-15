d0c48a8d22c10a01739640331b9b5a6eacb57da1
YuriiMisin <misin.yurii@phystech.edu>
2012-11-15 21:41:32 +0400

    Fixed

--- src/ru/fizteh/fivt/students/mysinYurii/shell/Shell.java
+++ src/ru/fizteh/fivt/students/mysinYurii/shell/Shell.java
@@ -1,23 +1,29 @@
-import java.util.Scanner;
+package ru.fizteh.fivt.students.mysinYurii.shell;
 
+import java.util.Scanner;
 
 public class Shell {
     public static void main(String[] args) {
         Executor runner = new Executor();
         if (args.length == 0) {
             Scanner inputData = new Scanner(System.in);
-            
             while (true) {
                 System.out.print("$ ");
                 String s = inputData.nextLine();
                 if (s == null) {
-                    System.out.println("Can't read input data");
+                    inputData.close();
+                    System.exit(0);
                 } else {
                     try {
                         runner.parseAndExec(s);
                     } catch(ShellException e) {
-                        System.out.println(e.getMessage());
-                        continue;
+                        if (e.getMessage().equals("exit")) {
+                            inputData.close();
+                            System.exit(0);
+                        } else {
+                            System.out.println(e.getMessage());
+                            continue;
+                        }
                     }
                 }
             }
