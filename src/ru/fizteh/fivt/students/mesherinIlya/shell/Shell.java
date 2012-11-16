58ecb299aaf046063ff3944e8b20f50be243dab0
syarik <mesherin.ilya@phystech.edu>
2012-11-17 02:36:35 +0400

    fixed bugs

--- src/ru/fizteh/fivt/students/mesherinIlya/shell/Shell.java
+++ src/ru/fizteh/fivt/students/mesherinIlya/shell/Shell.java
@@ -1,6 +1,7 @@
 
-import java.io.*;
+package ru.fizteh.fivt.students.mesherinIlya.shell;
 
+import java.io.*;
 
 public class Shell {
 
@@ -90,7 +91,7 @@ public class Shell {
                 
                 for (String entry : entries) {
                     File file = new File(entry);
-                    remove(file.getPath());
+                    remove(subject.getCanonicalPath() + File.separator + file.getName());
                 }
             }
             
@@ -107,7 +108,7 @@ public class Shell {
     }
 
     
-    static boolean copy(String file, String directory) {
+    static boolean copy(String file, String location) {
         try {
             File source = new File(file);
             if (!source.isAbsolute()) {
@@ -119,17 +120,16 @@ public class Shell {
                 return false;
             }    
             
-            File destination = new File(directory);
+            File destination = new File(location);
             if (!destination.isAbsolute()) {
-                destination = new File(currentDir.getPath() + File.separator + directory);
+                destination = new File(currentDir.getPath() + File.separator + location);
             }
 
-            if (!destination.isDirectory()) {
-                System.err.println("Cannot copy to \'" + directory + "\': the directory doesn't exist");
-                return false;
-            }
-            
             if (source.isDirectory()) {
+                if (!destination.isDirectory()) {
+                    mkdir(destination.getCanonicalPath());    
+                }
+                           
                 File newDestination = new File(destination + File.separator + source.getName());
                 if (!newDestination.exists()) {
                     newDestination.mkdir();
@@ -148,7 +148,12 @@ public class Shell {
                 FileOutputStream output = null;
                 try {
                     input = new FileInputStream(source);
-                    output = new FileOutputStream(destination + File.separator + source.getName());
+                    if (destination.isDirectory()) {
+                        output = new FileOutputStream(destination + File.separator + source.getName());
+                    } else {
+                        output = new FileOutputStream(destination);
+                    }
+                    
                     int count;
                     byte[] buf = new byte[16];  
                     while (true) {
@@ -173,8 +178,8 @@ public class Shell {
         return true;    
     }
     
-    static boolean move(String file, String directory) {
-        if (!copy(file, directory) || !remove(file)) {
+    static boolean move(String file, String location) {
+        if (!copy(file, location) || !remove(file)) {
             return false;
         }
     
@@ -198,69 +203,74 @@ public class Shell {
     static boolean executeCommand(String command) {
         String[] tokens = command.split("[\\s]+");
         
-        if (tokens == null) {
+        if (tokens == null || tokens.length == 0) {
+            return true;
+        }
+        if (tokens.length == 1 && tokens[0].equals("")) {
             return true;
         }
         
-        if (tokens[0].equals("cd")) {
-            if (tokens.length == 2) {
-                return cd(tokens[1]);
+        int i = tokens[0].equals("") ? 1 : 0;
+        
+        if (tokens[i].equals("cd")) {
+            if (tokens.length == i + 2) {
+                return cd(tokens[i + 1]);
             }
             else {
                 System.err.println("Error: cd: wrong count of arguments");
                 return false;
             }
         }
-        else if (tokens[0].equals("pwd")) {
-            if (tokens.length == 1) {
+        else if (tokens[i].equals("pwd")) {
+            if (tokens.length == i + 1) {
                 return pwd();
             }
             else {
                 System.err.println("Error: pwd: wrong count of arguments");
                 return false;
             }
-        } else if (tokens[0].equals("cp")) {
-            if (tokens.length == 3) {
-                return copy(tokens[1], tokens[2]);
+        } else if (tokens[i].equals("cp")) {
+            if (tokens.length == i + 3) {
+                return copy(tokens[i + 1], tokens[i + 2]);
             }
             else {
                 System.err.println("Error: cp: wrong count of arguments");
                 return false;
             }
-        } else if (tokens[0].equals("mv")) {
-            if (tokens.length == 3) {
-                return move(tokens[1], tokens[2]);
+        } else if (tokens[i].equals("mv")) {
+            if (tokens.length == i + 3) {
+                return move(tokens[i + 1], tokens[i + 2]);
             }
             else {
                 System.err.println("Error: mv: wrong count of arguments");
                 return false;
             }
-        } else if (tokens[0].equals("rm")) {
-            if (tokens.length == 2) {
-                return remove(tokens[1]);
+        } else if (tokens[i].equals("rm")) {
+            if (tokens.length == i + 2) {
+                return remove(tokens[i + 1]);
             }
             else {
                 System.err.println("Error: rm: wrong count of arguments");
                 return false;
             }
-        } else if (tokens[0].equals("dir")) {
-            if (tokens.length == 1) {
+        } else if (tokens[i].equals("dir")) {
+            if (tokens.length == i + 1) {
                 return dir();
             }
             else {
                 System.err.println("Error: dir: wrong count of arguments");
                 return false;
             }
-        } else if (tokens[0].equals("mkdir")) {
-            if (tokens.length == 2) {
-                return mkdir(tokens[1]);
+        } else if (tokens[i].equals("mkdir")) {
+            if (tokens.length == i + 2) {
+                return mkdir(tokens[i + 1]);
             }
             else {
                 System.err.println("Error: mkdir: wrong count of arguments");
                 return false;
             }
-        } else if (tokens[0].equals("exit")) {
-            if (tokens.length == 1) {
+        } else if (tokens[i].equals("exit")) {
+            if (tokens.length == i + 1) {
                 System.exit(0);
             }
             else {
@@ -281,12 +291,16 @@ public class Shell {
                 System.out.print("$ ");
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                 String string = reader.readLine();
-                if (string == null) {
+                
+                String[] commands = string.split(";");
+                if (commands == null) {
                     continue;
                 }
                 
-                String[] commands = string.split(";");
                 for (String command: commands) {
+                    if (command.equals("")) {
+                        continue;
+                    }
                     if (!executeCommand(command)) {
                         break;
                     }
@@ -296,9 +310,6 @@ public class Shell {
                 System.err.println("Fatal error. Program has been interrupted.");
                 System.exit(1);
             }        
-            
-            
-        
         }
     }
     
@@ -318,18 +329,10 @@ public class Shell {
             String[] commands = string.split(";");           
             for (String command : commands) {
                 if (!executeCommand(command)) {
-                    System.err.println("Program has been interrupted.");
                     System.exit(1);
                 }
             }
         }
-        
-        
-    
     }
-
-
-
-
 
 }
