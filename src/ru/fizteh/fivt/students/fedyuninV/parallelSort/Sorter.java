3e673157cd7de6a3c9e2a18958f430f93aaa8c44
Kashinskaya Yana <kashin.yana@gmail.com>
2012-11-17 19:39:45 +0400

    delete optimiz

---
+++ src/ru/fizteh/fivt/students/fedyuninV/parallelSort/Sorter.java
@@ -0,0 +1,11 @@
+package ru.fizteh.fivt.students.fedyuninV.parallelSort;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yana
+ * Date: 17.11.12
+ * Time: 19:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sorter {
+}
