a85d0599a446a951e829895e648a2d9faed3ec7c
Dmitriy Belyakov <dmitriy.b11@gmail.com>
2012-11-17 17:12:34 +0400

    Empty classes committed

--- src/ru/fizteh/fivt/students/dmitriyBelyakov/xmlBinder/XmlBinderTest.java
+++ src/ru/fizteh/fivt/students/dmitriyBelyakov/xmlBinder/XmlBinderTest.java
@@ -2,5 +2,5 @@ package ru.fizteh.fivt.students.dmitriyBelyakov.xmlBinder;
 
 import org.junit.Assert;
 
-public class XmlBinderUnitTests extends Assert {
+public class XmlBinderTest extends Assert {
 }
