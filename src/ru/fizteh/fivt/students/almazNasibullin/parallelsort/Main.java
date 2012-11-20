fb122fe8ce26c5ad390ac6931f6913d01f143068
AlmazNasibullin <nasibyllin.almaz@phystech.edu>
2012-11-20 22:24:22 +0400

    fix parallelSort

--- src/ru/fizteh/fivt/students/almazNasibullin/parallelsort/Main.java
+++ src/ru/fizteh/fivt/students/almazNasibullin/parallelsort/Main.java
@@ -7,8 +7,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
 import ru.fizteh.fivt.students.almazNasibullin.IOUtils;
@@ -19,14 +21,18 @@ import ru.fizteh.fivt.students.almazNasibullin.IOUtils;
  */
 
 public class Main {
-    // соответствует ключу -i
-    private static boolean withoutReg = false;
     // соответствует ключу -u
     private static boolean uniqLines = false;
     // кол-во потоков
     private static int countThreads = 0;
     // имя файла, в который необходимо вывести результат
     private static String outputFile = "";
+    private static Comparator<String> com = new Comparator<String>() {
+        @Override
+        public int compare(String s1, String s2) {
+            return s1.compareTo(s2);
+        }
+    };
 
     public static void main(String[] args) {
         // файлы для считывания
@@ -41,7 +47,7 @@ public class Main {
 
         // считывание строк
         lines = readLines(files);
-
+        
         files.clear();
 
         int size = lines.size();
@@ -72,18 +78,14 @@ public class Main {
             
             // работа с потоками: слияние
             List<String> res = startingThreadsToMerge(result);
-
+            
             printResult(res);
             res.clear();
         } else {
             // работа без запуска дополнительных потоков по причине малого
             // количества входных строчек
 
-            if (withoutReg) {
-                Collections.sort(lines, String.CASE_INSENSITIVE_ORDER);
-            } else {
-                Collections.sort(lines);
-            }
+            Collections.sort(lines, com);
             
             printResult(lines);
             lines.clear();
@@ -109,13 +111,13 @@ public class Main {
             String str = st.nextToken();
             if (str.equals("-i")) {
                 checkOrderArguments(files);
-                withoutReg = true;
+                com = String.CASE_INSENSITIVE_ORDER;
             } else if (str.equals("-u")) {
                 checkOrderArguments(files);
                 uniqLines = true;
             } else if (str.equals("-ui") || str.equals("-iu")) {
                 checkOrderArguments(files);
-                withoutReg = true;
+                com = String.CASE_INSENSITIVE_ORDER;
                 uniqLines = true;
             } else if (str.equals("-t")) {
                 if (files.isEmpty()) {
@@ -179,10 +181,10 @@ public class Main {
                 IOUtils.closeOrExit(br);
             }
         } else { // считывание из файлов
-            for (int i = 0; i < files.size(); ++i) {
+            for (String file : files) {
                 FileReader fr = null;
                 try {
-                    fr = new FileReader(new File(files.get(i)));
+                    fr = new FileReader(new File(file));
                     br = new BufferedReader(fr);
                     readFromBufferedReader(br, false, fr, lines);
                 } catch (Exception e) {
@@ -209,23 +211,19 @@ public class Main {
             int end = size / countThreads - 1;
 
             for (int i = 0; i < countThreads; ++i) {
-                if (i == 0 && countThreads > 3) {
-                    end *= 7;
-                    end /= 5;
-                }
                 if (i + 1 == countThreads) {
                     end = size - 1;
                 }
-                Sorter s = new Sorter(lines, start, end, result.get(i), withoutReg);
+                Sorter s = new Sorter(lines, start, end, result.get(i), com);
                 threads.add(new Thread(s));
                 threads.get(i).start();
                 start = end + 1;
                 end += size / countThreads;
             }
 
-            for (int i = 0; i < threads.size(); ++i) {
+            for (Thread t : threads) {
                 try {
-                    threads.get(i).join();
+                    t.join();
                 } catch (Exception e) {
                     IOUtils.printErrorAndExit("Bad joining: " + e.getMessage());
                 }
@@ -258,20 +256,20 @@ public class Main {
                     if (i == 0 && size % 2 != 0) {
                         m = new Merger(res.get(i), result, start, end, 0,
                                 result.get(start).size() - 1, 0,
-                                result.get(end).size() / 2, withoutReg);
+                                result.get(end).size() / 2, com);
                         start += 1;
                         end += 1;
                     } else if (i == 1 && size % 2 != 0) {
                         m = new Merger(res.get(i), result, start, end,
                                 result.get(start).size() / 2 + 1,
                                 result.get(start).size() - 1, 0,
-                                result.get(end).size() - 1, withoutReg);
+                                result.get(end).size() - 1, com);
                         start += 2;
                         end += 2;
                     } else {
                         m = new Merger(res.get(i), result, start, end, 0,
                                 result.get(start).size() - 1, 0,
-                                result.get(end).size() - 1, withoutReg);
+                                result.get(end).size() - 1, com);
                         start += 2;
                         end += 2;
                     }
@@ -279,9 +277,9 @@ public class Main {
                     threads.get(i).start();
                 }
 
-                for (int i = 0; i < curSize; ++i) {
+                for (Thread t : threads) {
                     try {
-                        threads.get(i).join();
+                        t.join();
                     } catch (Exception e) {
                         IOUtils.printErrorAndExit("Bad joining: " + e.getMessage());
                     }
@@ -301,81 +299,139 @@ public class Main {
         }
         return result.get(0);
     }
-    
+
+    /*public static void removeAndAdd(List<List<String> > result, List<String> res,
+            TreeMap<String, List<Pair> > tm) {
+        // достаем строчку наименьшую из tm и добавляем в res
+        String cur = tm.firstKey();
+        Pair p = tm.get(cur).get(0);
+        res.add(result.get(p.first).get(p.second));
+        tm.get(cur).remove(p);
+        if (tm.get(cur).isEmpty()) {
+            tm.remove(cur);
+        }
+
+        int index = p.second + 1;
+        if (withoutReg) {
+            if (index < result.get(p.first).size()) {
+                if( !cur.equalsIgnoreCase(
+                    result.get(p.first).get(index))) {
+                        cur = result.get(p.first).get(index);
+                        while (index < result.get(p.first).size() && cur.equalsIgnoreCase(
+                            result.get(p.first).get(index))) {
+                        String toAdd = result.get(p.first).get(index);
+                        if (!tm.containsKey(toAdd)) {
+                            tm.put(toAdd, new ArrayList<Pair>());
+                        }
+                        tm.get(toAdd).add(new Pair(p.first, index));
+                        ++index;
+                    }
+                }
+            }
+        } else {
+            if (index < result.get(p.first).size()) {
+                if( !cur.equals(
+                    result.get(p.first).get(index))) {
+                    cur = result.get(p.first).get(index);
+                    while (index < result.get(p.first).size() && cur.equals(
+                            result.get(p.first).get(index))) {
+                        String toAdd = result.get(p.first).get(index);
+                        if (!tm.containsKey(toAdd)) {
+                            tm.put(toAdd, new ArrayList<Pair>());
+                        }
+                        tm.get(toAdd).add(new Pair(p.first, index));
+                        ++index;
+                    }
+                }
+            }
+        }
+    }
+
+    public static List<String> MergeResult(List<List<String> > result) {
+        // res - результат слияния result
+        TreeMap<String, List<Pair> > tm;
+        List<String> res= new ArrayList();
+
+        if (withoutReg) {
+            tm = new TreeMap<String, List<Pair> >(
+                    String.CASE_INSENSITIVE_ORDER);
+        } else {
+            tm = new TreeMap<String, List<Pair> >();
+        }
+            
+        for (int i = 0; i < result.size(); ++i) {
+            String cur = result.get(i).get(0);
+            int index = 0;
+
+            if (withoutReg) {
+                while (index < result.get(i).size() && cur.equalsIgnoreCase(
+                        result.get(i).get(index))) {
+                    String toAdd = result.get(i).get(index);
+                    if (!tm.containsKey(toAdd)) {
+                        tm.put(toAdd, new ArrayList<Pair>());
+                    }
+                    tm.get(toAdd).add(new Pair(i, index));
+                    ++index;
+                }
+            } else {
+                while (index < result.get(i).size() && cur.equals(
+                        result.get(i).get(index))) {
+                    String toAdd = result.get(i).get(index);
+                    if (!tm.containsKey(toAdd)) {
+                        tm.put(toAdd, new ArrayList<Pair>());
+                    }
+                    tm.get(toAdd).add(new Pair(i, index));
+                    ++index;
+                }
+            }
+        }
+
+        while (!tm.isEmpty()) {
+            removeAndAdd(result, res, tm);
+        }
+        tm.clear();
+        return res;
+    }*/
+
     public static void writeLineToBufferedWriter(BufferedWriter bw,
-            List<String> lines, int i) {
+            String s) {
         try {
-            bw.write(lines.get(i) + "\n", 0, lines.get(i).length() + 1);
-            bw.flush();
+            bw.write(s + "\n", 0, s.length() + 1);
         } catch (IOException e) {
             IOUtils.printErrorAndExit("Bad writing to BufferedWriter: " + e.getMessage());
         }
     }
 
     public static void printResult(List<String> lines) {
-        int size = lines.size();
-        if (outputFile.equals("")) { // вывод в stdout
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        try {
+            if (outputFile.equals("")) {
+                bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            } else {
+                fw = new FileWriter(new File(outputFile));
+                bw = new BufferedWriter(fw);
+            }
             if (uniqLines) { // вывод уникальных строк
-                String prev;
-                if (withoutReg) {
-                    prev = lines.get(0).toLowerCase();
-                } else {
-                    prev = lines.get(0);
-                }
-                System.out.println(lines.get(0));
-                for (int i = 1; i < size; ++i) {
-                    if (withoutReg) {
-                        if (!lines.get(i).toLowerCase().equals(prev)) {
-                            System.out.println(lines.get(i));
-                            prev = lines.get(i).toLowerCase();
-                        }
-                    } else {
-                        if (!lines.get(i).equals(prev)) {
-                            System.out.println(lines.get(i));
-                            prev = lines.get(i);
-                        }
+                String prev = lines.get(0);
+                writeLineToBufferedWriter(bw, prev);
+
+                for (String s: lines) {
+                    if (com.compare(prev, s) != 0) {
+                        writeLineToBufferedWriter(bw, s);
+                        prev = s;
                     }
                 }
             } else {
-                for (int i = 0; i < size; ++i) {
-                    System.out.println(lines.get(i));
+                for (String s: lines) {
+                    writeLineToBufferedWriter(bw, s);
                 }
             }
-        } else { // вывод в файл
-            BufferedWriter bw = null;
-            FileWriter fw = null;
-            try {
-                fw = new FileWriter(new File(outputFile));
-                bw = new BufferedWriter(fw);
-                if (uniqLines) { // вывод уникальных строк
-                    writeLineToBufferedWriter(bw, lines, 0);
-                    String prev;
-                    if (withoutReg) {
-                        prev = lines.get(0).toLowerCase();
-                    } else {
-                        prev = lines.get(0);
-                    }
-                    for (int i = 1; i < size; ++i) {
-                        if (withoutReg) {
-                            if (!lines.get(i).toLowerCase().equals(prev)) {
-                                writeLineToBufferedWriter(bw, lines, i);
-                                prev = lines.get(i).toLowerCase();
-                            }
-                        } else {
-                            if (!lines.get(i).equals(prev)) {
-                                writeLineToBufferedWriter(bw, lines, i);
-                                prev = lines.get(i);
-                            }
-                        }
-                    }
-                } else {
-                    for (int i = 0; i < size; ++i) {
-                        writeLineToBufferedWriter(bw, lines, i);
-                    }
-                }
-            } catch (Exception e) {
-                IOUtils.printErrorAndExit(e.getMessage());
-            } finally {
+            bw.flush();
+        } catch (Exception e) {
+            IOUtils.printErrorAndExit(e.getMessage());
+        } finally {
+            if (!(outputFile.equals(""))) {
                 IOUtils.closeOrExit(fw);
                 IOUtils.closeOrExit(bw);
             }
