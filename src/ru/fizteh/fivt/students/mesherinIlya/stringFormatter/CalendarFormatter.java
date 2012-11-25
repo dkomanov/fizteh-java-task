adf3d184a06af9b568a3df11d16b36e55f9ff8c5
syarik <mesherin.ilya@phystech.edu>
2012-11-26 03:12:14 +0400

    string formatter

---
+++ src/ru/fizteh/fivt/students/mesherinIlya/stringFormatter/CalendarFormatter.java
@@ -0,0 +1,43 @@
+package ru.fizteh.fivt.students.mesherinIlya.stringFormatter;
+
+import ru.fizteh.fivt.format.FormatterException;
+import ru.fizteh.fivt.format.StringFormatterExtension;
+    
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class CalendarFormatter extends StringFormatterExtension {
+    CalendarFormatter() {
+        super(Calendar.class);
+    }
+
+    @Override
+    public void format(StringBuilder buffer, Object object, String pattern) throws FormatterException {
+        try {
+            if (pattern.equals("")) {
+                throw new FormatterException("Empty pattern.");
+            }
+            if (buffer == null) {
+                throw new FormatterException("Buffer is null.");
+            }
+            if (pattern == null) {
+                throw new FormatterException("Pattern is null.");
+            }
+            if (object == null) {
+                throw new FormatterException("Calendar is null.");
+            }
+            if (!Calendar.class.isAssignableFrom(object.getClass())) {
+                throw new FormatterException("Incorrect object type.");
+            }
+            SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+            buffer.append(formatter.format(((Calendar) object).getTime()));
+        } catch (Throwable t) {
+            throw new FormatterException(t.getMessage(), t);
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return this.clazz.isAssignableFrom(clazz);
+    }
+}
