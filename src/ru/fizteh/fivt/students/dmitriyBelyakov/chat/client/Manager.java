4af058c693ae3befd2122f99fd4e01d89c67da85
Dmitriy Belyakov <dmitriy.b11@gmail.com>
2012-11-25 21:34:13 +0400

    Delete changes in another file

--- src/ru/fizteh/fivt/students/dmitriyBelyakov/chat/client/Manager.java
+++ src/ru/fizteh/fivt/students/dmitriyBelyakov/chat/client/Manager.java
@@ -60,13 +60,15 @@ class Manager {
     }
 
     synchronized public void delete(ServerWorker server) {
-        servers.remove(server);
         if (server == currentWorker) {
             currentWorker = servers.size() > 0 ? servers.get(0) : null;
             if (currentWorker != null) {
-                currentWorker.activate();
+                if (currentWorker != null) {
+                    currentWorker.activate();
+                }
             }
         }
+        servers.remove(server);
         System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date().getTime()) + "] Closed connection with server '" + server.name() + "'.");
     }
 
