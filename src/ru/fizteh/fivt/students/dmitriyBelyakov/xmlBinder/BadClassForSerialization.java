07183a6d3e78fff86c7354a1fbe7f5ff8406c9ad
Dmitriy Komanov <dkomanov@gmail.com>
2012-11-27 03:04:06 +0400

    Removed to fix compilation

--- src/ru/fizteh/fivt/students/dmitriyBelyakov/xmlBinder/BadClassForSerialization.java
+++
@@ -1,9 +0,0 @@
-package ru.fizteh.fivt.students.dmitriyBelyakov.xmlBinder;
-
-public class BadClassForSerialization {
-    private BadClassForSerialization link;
-
-    BadClassForSerialization() {
-        link = this;
-    }
-}
