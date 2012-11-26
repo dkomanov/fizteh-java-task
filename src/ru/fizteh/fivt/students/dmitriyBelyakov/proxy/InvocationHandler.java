2ce7ee971647e8799d9410b336973e7e09183ebc
Dmitriy Belyakov <dmitriy.b11@gmail.com>
2012-11-26 15:07:43 +0400

    Code convention

--- src/ru/fizteh/fivt/students/dmitriyBelyakov/proxy/InvocationHandler.java
+++ src/ru/fizteh/fivt/students/dmitriyBelyakov/proxy/InvocationHandler.java
@@ -16,31 +16,31 @@ public class InvocationHandler implements java.lang.reflect.InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Exception {
-        if(method.getAnnotation(DoNotProxy.class) != null) {
+        if (method.getAnnotation(DoNotProxy.class) != null) {
             throw new RuntimeException("Incorrect method.");
         }
-        if(method.getAnnotation(Collect.class) != null) {
+        if (method.getAnnotation(Collect.class) != null) {
             Class returnType = method.getReturnType();
-            if(returnType.equals(void.class)) {
-                for(Object target : targets) {
+            if (returnType.equals(void.class)) {
+                for (Object target : targets) {
                     method.invoke(target, args);
                 }
                 return null;
-            } else if(returnType.equals(int.class) || returnType.equals(Integer.class)) {
+            } else if (returnType.equals(int.class) || returnType.equals(Integer.class)) {
                 int result = 0;
-                for(Object target : targets) {
+                for (Object target : targets) {
                     result += (Integer) method.invoke(target, args);
                 }
                 return result;
-            } else if(returnType.equals(long.class) || returnType.equals(Long.class)) {
+            } else if (returnType.equals(long.class) || returnType.equals(Long.class)) {
                 long result = 0;
-                for(Object target : targets) {
+                for (Object target : targets) {
                     result += (Long) method.invoke(target, args);
                 }
                 return result;
-            } else if(returnType.isAssignableFrom(List.class)) {
+            } else if (returnType.isAssignableFrom(List.class)) {
                 List result = new ArrayList();
-                for(Object target : targets) {
+                for (Object target : targets) {
                     result.addAll((List) method.invoke(target, args));
                 }
                 return result;
