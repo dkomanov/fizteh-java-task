7f111754aa426cf62ffd7e6a5174f3d5e2b45221
kogemrka <kogemrka@gmail.com>
2012-11-29 01:53:22 +0400

    Наброски фабрики

--- src/ru/fizteh/fivt/students/nikitaAntonov/stringformatter/StringFormatterFactory.java
+++ src/ru/fizteh/fivt/students/nikitaAntonov/stringformatter/StringFormatterFactory.java
@@ -1,15 +1,51 @@
 package ru.fizteh.fivt.students.nikitaAntonov.stringformatter;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import ru.fizteh.fivt.format.FormatterException;
 import ru.fizteh.fivt.format.StringFormatter;
+import ru.fizteh.fivt.format.StringFormatterExtension;
 
-public class StringFormatterFactory implements ru.fizteh.fivt.format.StringFormatterFactory {
+public class StringFormatterFactory implements
+        ru.fizteh.fivt.format.StringFormatterFactory {
+    private Map<String, StringFormatterExtension> knownsExtensions;
+
+    public StringFormatterFactory() {
+        knownsExtensions = Collections
+                .synchronizedMap(new HashMap<String, StringFormatterExtension>());
+    }
 
     @Override
     public StringFormatter create(String... extensionClassNames)
             throws FormatterException {
-        // TODO Auto-generated method stub
-        return null;
-    }
 
+        ArrayList<StringFormatterExtension> extensionsList = new ArrayList<>();
+
+        try {
+            for (String extensionName : extensionClassNames) {
+                if (extensionName == null) {
+                    throw new NullPointerException();
+                }
+
+                StringFormatterExtension extension = knownsExtensions
+                        .get(extensionName);
+
+                if (extension == null) {
+                    extension = (StringFormatterExtension) Class.forName(
+                            extensionName).newInstance();
+                    
+                    knownsExtensions.put(extensionName, extension);
+                }
+
+                extensionsList.add(extension);
+            }
+        } catch (Throwable e) {
+            throw new FormatterException("Incorrect extension", e);
+        }
+
+        return new StringFormatter(extensionsList);
+    }
 }
