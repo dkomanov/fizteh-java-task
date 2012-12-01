12b64343600ae77a7e452d07d75e156b6cc6e15d
Zakhar Pilipchuk <z.pilipchuk@gmail.com>
2012-12-01 14:55:44 +0400

    Fixed international problems:)

--- src/ru/fizteh/fivt/students/harius/calendar/CalendarView.java
+++ src/ru/fizteh/fivt/students/harius/calendar/CalendarView.java
@@ -76,14 +76,11 @@ public class CalendarView {
 	/* Writes the main part */
 	private void generateBody(StringBuilder builder) {
 		Calendar iterator = Calendar.getInstance(locale);
+		iterator.setFirstDayOfWeek(MONDAY);
 		iterator.setTime(calendar.getTime());
 
 		iterator.set(DAY_OF_MONTH,
 			iterator.getActualMinimum(DAY_OF_MONTH));
-
-		if (iterator.get(DAY_OF_WEEK) == SUNDAY) {
-			iterator.set(WEEK_OF_YEAR, iterator.get(WEEK_OF_YEAR) - 1);
-		}
 
 		iterator.set(DAY_OF_WEEK, MONDAY);
 
