d71f7e11853aa97e3d097e69ee0acdc9d965e2bb
Nikolay Frolov <frolov.nikolay@phystech.edu>
2012-12-01 16:32:02 +0400

    Добавлен xmlBinder

---
+++ src/ru/fizteh/fivt/students/frolovNikolay/xmlBinder/XmlTests.java
@@ -0,0 +1,21 @@
+package ru.fizteh.fivt.students.frolovNikolay.xmlBinder;
+
+import ru.fizteh.fivt.bind.test.Permissions;
+import ru.fizteh.fivt.bind.test.User;
+import ru.fizteh.fivt.bind.test.UserName;
+import ru.fizteh.fivt.bind.test.UserType;
+
+public class XmlTests {
+    
+    public static void main(String[] args) throws Throwable {
+        XmlBinder<User> binder = new XmlBinder<User>(User.class);
+        Permissions permissions = new Permissions();
+        permissions.setQuota(100500);
+        User user = new User(1, UserType.USER, new UserName("first", "last"), permissions);
+        byte[] bytes = binder.serialize(user);
+        User deserialized = binder.deserialize(bytes);
+        assert user != deserialized;
+        assert user.equals(deserialized);
+    }
+    
+}
