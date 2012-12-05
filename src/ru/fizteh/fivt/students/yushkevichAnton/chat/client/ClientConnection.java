f8e3642ae03e1df576d27c78a0873defb66347fe
Anton Yushkevich <ushkevich.anton@phystech.edu>
2012-12-05 23:27:38 +0400

    Chat

---
+++ src/ru/fizteh/fivt/students/yushkevichAnton/chat/client/ClientConnection.java
@@ -0,0 +1,82 @@
+package ru.fizteh.fivt.students.yushkevichAnton.chat.client;
+
+import java.io.*;
+import java.net.ProtocolException;
+import java.net.Socket;
+
+class ClientConnection {
+    private Server server;
+    private String nickName;
+    private CommunicationThread communicationThread;
+    private boolean alive = true;
+
+    boolean isAlive() {
+        return alive;
+    }
+
+    void setNickName(String nickName) throws IOException {
+        if (!server.isCorrectNickName(nickName)) {
+            throw new ProtocolException("That nickname is already taken");
+        }
+
+        this.nickName = nickName;
+    }
+
+    String getNickName() {
+        return nickName;
+    }
+
+    ClientConnection(Socket socket, Server server) {
+        this.server = server;
+
+        try {
+            communicationThread = new CommunicationThread(socket, this);
+            communicationThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            disconnect(e.getMessage());
+        }
+    }
+
+    void disconnect() {
+        disconnect(null);
+    }
+
+    void disconnect(String message) {
+        alive = false;
+
+        if (message != null) {
+            communicationThread.sendError(message);
+        }
+
+        communicationThread.sendBye();
+
+        if (communicationThread != null) {
+            communicationThread.interrupt();
+        }
+
+        server.validateClients();
+    }
+
+    void announce(String message) {
+        server.announce(nickName, message);
+    }
+
+    void sendMessage(String nickName, String message) {
+        try{
+            communicationThread.sendMessage(nickName, message);
+        } catch (IOException e) {
+            disconnect();
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return nickName.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return this == object;
+    }
+}
---
+++ src/ru/fizteh/fivt/students/yushkevichAnton/chat/client/ServerConnection.java
@@ -0,0 +1,88 @@
+package ru.fizteh.fivt.students.yushkevichAnton.chat.client;
+
+import java.net.Socket;
+
+class ServerConnection {
+    private String nickName;
+    private Client client;
+
+    private String id;
+
+    private boolean alive = true;
+
+    private CommunicationThread communicationThread;
+
+    ServerConnection(String address, int port, String nickName, Client client) {
+        this.nickName = nickName;
+        this.client = client;
+
+        try {
+            Socket socket = new Socket(address, port);
+
+            id = socket.getInetAddress().toString() + ":" + socket.getPort();
+
+            communicationThread = new CommunicationThread(socket, this);
+            communicationThread.start();
+        } catch (Exception e) {
+            disconnect();
+        }
+    }
+
+    boolean isAlive() {
+        return alive;
+    }
+
+    boolean isCurrentServer() {
+        return client.isCurrentServer(this);
+    }
+
+    String getNickName() {
+        return nickName;
+    }
+
+    void disconnect() {
+        if (!alive) {
+            return;
+        }
+
+        alive = false;
+
+        try {
+            communicationThread.bye();
+        } catch (Exception e) {
+        }
+
+        communicationThread.flush();
+        communicationThread.interrupt();
+
+        System.out.println("You got disconnected from " + this);
+
+        client.validateServers();
+    }
+
+    void post(String message) {
+        try {
+            communicationThread.post(message);
+        } catch (Exception e) {
+            disconnect();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this == obj;
+    }
+}
+It works
+latest commit 1c89d0506f
+ Abrackadabra authored 3 hours ago
+..		
+ClientConnection.java	3 hours ago	It works [Abrackadabra]
+CommunicationThread.java	3 hours ago	It's alive! [Abrackadabra]
+ListeningThread.java	3 hours ago	It works [Abrackadabra]
+Server.java	3 hours ago	It works [Abrackadabra]
