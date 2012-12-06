762e2962e17333676072d51d982158ecfd1cd76f
Dmitriy Belyakov <dmitriy.b11@gmail.com>
2012-12-07 01:08:30 +0400

    Changes

--- src/ru/fizteh/fivt/students/dmitriyBelyakov/proxy/asmProxy/AsmShardingProxyFactory.java
+++ src/ru/fizteh/fivt/students/dmitriyBelyakov/proxy/asmProxy/AsmShardingProxyFactory.java
@@ -7,12 +7,13 @@ import ru.fizteh.fivt.students.dmitriyBelyakov.proxy.ProxyUtils;
 
 import java.io.PrintStream;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class AsmShardingProxyFactory implements ShardingProxyFactory {
     @Override
-    public Object createProxy(Object[] targets, Class[] interfaces) {
+    public Object createProxy(final Object[] targets, Class[] interfaces) {
         ProxyUtils.throwExceptionIsArgumentsIsIncorrect(targets, interfaces);
         ClassWriter cw = newClassWriter();
         String[] interfacesName = new String[interfaces.length];
@@ -20,7 +21,7 @@ public class AsmShardingProxyFactory implements ShardingProxyFactory {
             interfacesName[i] = Type.getInternalName(interfaces[i]);
         }
         cw.visit(Opcodes.V1_7, Opcodes.ACC_PUBLIC, "Proxy", null, "java/lang/Object", interfacesName);
-        cw.visitField(Opcodes.ACC_PRIVATE, "targets", Type.getDescriptor(List.class), null, null).visitEnd();
+        cw.visitField(Opcodes.ACC_PRIVATE, "targets", Type.getDescriptor(ArrayList.class), null, null).visitEnd();
         generateMethod(cw, Opcodes.ACC_PUBLIC, "<init>", "()V",
                 new Function1V<GeneratorAdapter>() {
                     @Override
@@ -38,7 +39,7 @@ public class AsmShardingProxyFactory implements ShardingProxyFactory {
                         ga.returnValue();
                     }
                 });
-        generateMethod(cw, Opcodes.ACC_PUBLIC, "loadTargets", "(" + Type.getDescriptor(List.class) + ")V",
+        generateMethod(cw, Opcodes.ACC_PUBLIC, "loadTargets", "(" + Type.getDescriptor(ArrayList.class) + ")V",
                 new Function1V<GeneratorAdapter>() {
                     @Override
                     public void apply(GeneratorAdapter ga) {
@@ -49,15 +50,21 @@ public class AsmShardingProxyFactory implements ShardingProxyFactory {
                         ga.push("Hello, World!!!");
                         ga.invokeVirtual(printStreamType, new org.objectweb.asm.commons.Method("println", "(Ljava/lang/String;)V"));
                         ga.loadArg(0);
-                        ga.putField(Type.getType("Proxy"), "targets", Type.getType(List.class));
+                        ga.putField(Type.getType("Proxy"), "targets", Type.getType(ArrayList.class));
                         ga.returnValue();
                     }
                 });
-        /*for (Class interfc : interfaces) {
+        for (final Class interfc : interfaces) {
             Method[] methods = interfc.getDeclaredMethods();
-            for (Method method : methods) {
+            for (final Method method : methods) {
                 method.setAccessible(true);
                 String descriptor = Type.getMethodDescriptor(method);
+                try {
+                    interfc.getClass().getDeclaredMethod("cast", Object.class);
+                } catch (Throwable t) {
+                    System.out.println(interfc.getClass().getDeclaredMethods().length);
+                    while (true) ;
+                }
                 generateMethod(cw, Opcodes.ACC_PUBLIC, method.getName(), "()V", // TODO
                         new Function1V<GeneratorAdapter>() {
                             @Override
@@ -66,11 +73,19 @@ public class AsmShardingProxyFactory implements ShardingProxyFactory {
                                 ga.getStatic(Type.getType(System.class), "out", printStreamType);
                                 ga.push("Hello, World!");
                                 ga.invokeVirtual(printStreamType, new org.objectweb.asm.commons.Method("println", "(Ljava/lang/String;)V"));
+                                ga.getStatic(Type.getType(System.class), "out", printStreamType);
+                                ga.loadThis();
+                                ga.getField(Type.getType("Proxy"), "targets", Type.getType(ArrayList.class));
+                                ga.push(0);
+                                ga.invokeVirtual(Type.getType(ArrayList.class), new org.objectweb.asm.commons.Method("get",
+                                        "(" + Type.getDescriptor(int.class) + ")" + Type.getDescriptor(Object.class)));
+                                ga.invokeVirtual(Type.getType(targets[0].getClass()), new org.objectweb.asm.commons.Method(method.getName(),
+                                        Type.getMethodDescriptor(method)));
                                 ga.returnValue();
                             }
                         });
             }
-        }*/
+        }
         cw.visitEnd();
         try {
             Class clazz = loadClass(cw.toByteArray());
@@ -78,9 +93,9 @@ public class AsmShardingProxyFactory implements ShardingProxyFactory {
             System.out.println(obj);
             //System.out.println("||| " + clazz.getDeclaredMethod("loadTargets", Object[].class));
             //clazz.getDeclaredMethod("loadTargets", Object[].class).invoke(clazz.cast(obj), targets);
-            Method method = clazz.getDeclaredMethod("loadTargets", List.class);
+            Method method = clazz.getDeclaredMethod("loadTargets", ArrayList.class);
             System.out.println(method);
-            method.invoke(obj, Arrays.asList(targets));
+            method.invoke(obj, new ArrayList(Arrays.asList(targets)));
             return obj;
         } catch (Throwable t) {
             t.printStackTrace();
