2b3148f593ebaf20d6ecd7d9af9faf30aaa467e8
Dmitriy Belyakov <dmitriy.b11@gmail.com>
2012-12-08 02:16:39 +0400

    commit

--- src/ru/fizteh/fivt/students/dmitriyBelyakov/proxy/asmProxy/AsmShardingProxyFactory.java
+++ src/ru/fizteh/fivt/students/dmitriyBelyakov/proxy/asmProxy/AsmShardingProxyFactory.java
@@ -1,5 +1,6 @@
 package ru.fizteh.fivt.students.dmitriyBelyakov.proxy.asmProxy;
 
+import junit.framework.Assert;
 import org.objectweb.asm.*;
 import org.objectweb.asm.commons.GeneratorAdapter;
 import ru.fizteh.fivt.proxy.ShardingProxyFactory;
@@ -54,14 +55,17 @@ public class AsmShardingProxyFactory implements ShardingProxyFactory {
                             @Override
                             public void apply(GeneratorAdapter ga) {
                                 try {
+                                    System.out.println(Type.getMethodDescriptor(ProxyUtils.class.getDeclaredMethod("getFirstIntOrLongArgument", Object[].class)));
                                     ga.loadThis();
                                     ga.getField(Type.getType("Proxy"), "targets", Type.getType(ArrayList.class));
-                                    ga.loadArgs();
-                                    //ga.invokeStatic(Type.getType(ProxyUtils.class), new org.objectweb.asm.commons.Method("getFirstIntOrLongArgument",
-                                    //        Type.getMethodDescriptor(ProxyUtils.class.getDeclaredMethod("getFirstIntOrLongArgument", Object[].class))));
+                                    ga.loadArgArray();
+                                    ga.invokeStatic(Type.getType(ProxyUtils.class), new org.objectweb.asm.commons.Method("getFirstIntOrLongArgument",
+                                            Type.getMethodDescriptor(ProxyUtils.class.getDeclaredMethod("getFirstIntOrLongArgument", Object[].class))));
+                                    printTypeOnTheTop(ga);
+                                    ga.pop();
                                     ga.push(0);
-                                    //ga.invokeVirtual(Type.getType(ArrayList.class), new org.objectweb.asm.commons.Method("get",
-                                    //        "(" + Type.getDescriptor(int.class) + ")" + Type.getDescriptor(Object.class)));
+                                    ga.invokeVirtual(Type.getType(ArrayList.class), new org.objectweb.asm.commons.Method("get",
+                                            "(" + Type.getDescriptor(int.class) + ")" + Type.getDescriptor(Object.class)));
                                     ga.checkCast(Type.getType(interfc));
                                     ga.loadArgs();
                                     ga.invokeInterface(Type.getType(interfc), new org.objectweb.asm.commons.Method(method.getName(),
@@ -86,6 +90,16 @@ public class AsmShardingProxyFactory implements ShardingProxyFactory {
             t.printStackTrace();
             throw new RuntimeException("Cannot create proxy object.");
         }
+    }
+
+    private void printTypeOnTheTop(GeneratorAdapter ga) {
+        Type printStreamType = Type.getType(PrintStream.class);
+        ga.dup();
+        ga.getStatic(Type.getType(System.class), "out", printStreamType);
+        ga.swap(Type.getType(Object.class), printStreamType);
+        ga.invokeVirtual(Type.getType(Object.class), new org.objectweb.asm.commons.Method("getClass", "()" + Type.getDescriptor(Class.class)));
+        ga.invokeVirtual(Type.getType(Class.class), new org.objectweb.asm.commons.Method("getName", "()" + Type.getDescriptor(String.class)));
+        ga.invokeVirtual(printStreamType, new org.objectweb.asm.commons.Method("println", "(Ljava/lang/String;)V"));
     }
 
     private ClassWriter newClassWriter() {
