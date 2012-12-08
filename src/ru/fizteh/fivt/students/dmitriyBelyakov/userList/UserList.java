ef8bf5759b1c96eeb7f849ea3a078b9b3eae0c37
Dmitriy Belyakov <dmitriy.b11@gmail.com>
2012-12-08 12:40:49 +0400

    Added normal sorting

--- src/ru/fizteh/fivt/students/dmitriyBelyakov/userList/UserList.java
+++ src/ru/fizteh/fivt/students/dmitriyBelyakov/userList/UserList.java
@@ -26,41 +26,12 @@ class UserListTableModel extends AbstractTableModel {
     }
 
     @Override
-    public Class getColumnClass(int column) {
-        switch (column) {
-            case 0:
-                return Integer.class;
-            case 1:
-                return UserType.class;
-            case 2:
-                return String.class;
-            case 3:
-                return String.class;
-            case 4:
-                return Boolean.class;
-            case 5:
-                return Integer.class;
-            default:
-                return String.class;
-        }
-    }
-
-    public TableCellEditor getCellEditor(int row, int column) {
-        if (column == 1)
-            return editor;
-        else
-            return getCellEditor(row, column);
-    }
-
-    @Override
     public int getColumnCount() {
-        System.out.println(names.size());
         return names.size();
     }
 
     @Override
     public int getRowCount() {
-        System.out.println(users.size());
         return users.size();
     }
 
@@ -71,7 +42,6 @@ class UserListTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int row, int col) {
-        System.out.println(users.get(row).get(col));
         return users.get(row).get(col);
     }
 
@@ -83,50 +53,26 @@ class UserListTableModel extends AbstractTableModel {
     @Override
     public void setValueAt(Object value, int row, int col) {
         users.get(row).set(col, value);
+        fireTableDataChanged();
     }
-}
 
-class UserNameComparator implements Comparator<Vector<Object>> {
-    /*int firstNamePos;
-    int lastNamePos;
-
-    UserNameComparator(int firstNamePos, int lastNamePos) {
-        this.firstNamePos = firstNamePos;
-        this.lastNamePos = lastNamePos;
-    }*/
-
-    @Override
-    public int compare(Vector<Object> v1, Vector<Object> v2) {
-        if (v1.get(2).equals(v2.get(2))) {
-            return ((String) v1.get(3)).compareTo((String) v2.get(3));
-        } else {
-            return ((String) v1.get(2)).compareTo((String) v2.get(2));
-        }
+    public void removeRow(int row) {
+        users.remove(row);
+        fireTableDataChanged();
     }
-}
 
-class UserTypeComparator implements Comparator<Vector<Object>> {
-    @Override
-    public int compare(Vector<Object> v1, Vector<Object> v2) {
-        return ((UserType) v1.get(1)).compareTo((UserType) v2.get(1));
+    public void addRow(Vector<Object> row) {
+        users.add(row);
+        fireTableDataChanged();
     }
-}
 
-class UserNameComparatorRev implements Comparator<Vector<Object>> {
-    @Override
-    public int compare(Vector<Object> v2, Vector<Object> v1) {
-        if (v1.get(2).equals(v2.get(2))) {
-            return ((String) v1.get(3)).compareTo((String) v2.get(3));
-        } else {
-            return ((String) v1.get(2)).compareTo((String) v2.get(2));
-        }
+    public Vector<Vector<Object>> getData() {
+        return users;
     }
-}
 
-class UserTypeComparatorRev implements Comparator<Vector<Object>> {
-    @Override
-    public int compare(Vector<Object> v2, Vector<Object> v1) {
-        return ((UserType) v1.get(1)).compareTo((UserType) v2.get(1));
+    public void clear() {
+        users.clear();
+        fireTableDataChanged();
     }
 }
 
@@ -136,17 +82,10 @@ public class UserList extends JFrame {
     private JTable table;
     private XmlUserList xmlUserList;
     private File xmlFile;
-    private Vector<Vector<Object>> users;
-    private boolean namesSortedAscending;
-    private boolean typesSortedAscending;
-    Vector<String> names = new Vector<>();
 
     UserList() {
         super("UserList");
-        namesSortedAscending = false;
-        typesSortedAscending = false;
         xmlUserList = new XmlUserList();
-        users = new Vector<>();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(500, 700);
         createMenu();
@@ -158,7 +97,6 @@ public class UserList extends JFrame {
         try {
             UserList userList = new UserList();
         } catch (Throwable t) {
-            //t.printStackTrace();
             System.exit(1);
         }
     }
@@ -179,8 +117,6 @@ public class UserList extends JFrame {
                         updateTable(xmlUserList.loadUsers(xmlFile));
                     }
                 }
-                sortByNames();
-                sortByTypes();
             } else if (actionCommand.equals("SAVE")) {
                 try {
                     save();
@@ -200,33 +136,23 @@ public class UserList extends JFrame {
                         xmlFile = last;
                     }
                 }
-            } else if (actionCommand.equals("SORT_NAME")) {
-                sortByNames();
-            } else if (actionCommand.equals("SORT_TYPE")) {
-                sortByTypes();
             } else if (actionCommand.equals("NEW_USER")) {
                 Vector<Object> vector = new Vector<>();
                 vector.add(0);
-                vector.add(new String());
+                vector.add(UserType.USER);
                 vector.add(new String());
                 vector.add(new String());
                 vector.add(false);
                 vector.add(0);
-                users.add(vector);
-                sortByNames();
-                sortByTypes();
-                menu.updateUI();
+                ((UserListTableModel) table.getModel()).addRow(vector);
                 table.updateUI();
             } else if (actionCommand.equals("DELETE_USER")) {
                 int num = table.getSelectedRow();
                 if (num == -1) {
                     JOptionPane.showMessageDialog(frame, "No row has been selected.");
                 }
-                ((DefaultTableModel) table.getModel()).removeRow(num);
-                sortByNames();
-                sortByTypes();
+                ((UserListTableModel) table.getModel()).removeRow(num);
                 table.updateUI();
-                menu.updateUI();
             }
         }
 
@@ -236,6 +162,7 @@ public class UserList extends JFrame {
                 return;
             }
             ArrayList<User> usersList = new ArrayList<>();
+            Vector<Vector<Object>> users = ((UserListTableModel) table.getModel()).getData();
             for (Vector<Object> vector : users) {
                 int id = (Integer) vector.get(0);
                 UserType userType = (UserType) vector.get(1);
@@ -250,7 +177,7 @@ public class UserList extends JFrame {
         }
 
         public void updateTable(ArrayList<User> list) {
-            users.clear();
+            ((UserListTableModel) table.getModel()).clear();
             for (User user : list) {
                 if (user == null) {
                     continue;
@@ -272,14 +199,10 @@ public class UserList extends JFrame {
                 }
                 row.add(permissions.isRoot());
                 row.add(permissions.getQuota());
-                users.add(row);
+                ((UserListTableModel) table.getModel()).addRow(row);
             }
             table.updateUI();
         }
-    }
-
-    public Listener newListener() {
-        return new Listener();
     }
 
     private void createMenu() {
@@ -299,16 +222,6 @@ public class UserList extends JFrame {
         fileSaveAs.setActionCommand("SAVE_AS");
         fileSaveAs.addActionListener(listener);
         file.add(fileSaveAs);
-        JMenu sort = new JMenu("Sort");
-        JMenuItem sortName = new JMenuItem("Name");
-        sortName.setActionCommand("SORT_NAME");
-        sortName.addActionListener(listener);
-        sort.add(sortName);
-        JMenuItem sortType = new JMenuItem("Type");
-        sortType.setActionCommand("SORT_TYPE");
-        sortType.addActionListener(listener);
-        sort.add(sortType);
-        menu.add(sort);
         JMenu edit = new JMenu("Edit");
         JMenuItem editNewUser = new JMenuItem("New user");
         editNewUser.setActionCommand("NEW_USER");
@@ -319,40 +232,11 @@ public class UserList extends JFrame {
         editDeleteUser.addActionListener(listener);
         edit.add(editDeleteUser);
         menu.add(edit);
-        JMenu sortedByNames = new JMenu("");
-        menu.add(sortedByNames);
-        JMenu sortedByTypes = new JMenu("");
-        menu.add(sortedByTypes);
         setJMenuBar(menu);
     }
 
-    private void sortByNames() {
-        if (!namesSortedAscending) {
-            Collections.sort(users, new UserNameComparator());
-            namesSortedAscending = true;
-        } else {
-            Collections.sort(users, new UserNameComparatorRev());
-            namesSortedAscending = false;
-        }
-        menu.getMenu(3).setText("Names sorted: " + (namesSortedAscending ? "ascending." : "descending."));
-        menu.updateUI();
-        table.updateUI();
-    }
-
-    private void sortByTypes() {
-        if (!typesSortedAscending) {
-            Collections.sort(users, new UserTypeComparator());
-            typesSortedAscending = true;
-        } else {
-            Collections.sort(users, new UserTypeComparatorRev());
-            typesSortedAscending = false;
-        }
-        menu.getMenu(4).setText("Types sorted: " + (typesSortedAscending ? "ascending." : "descending."));
-        menu.updateUI();
-        table.updateUI();
-    }
-
     private void createTable() {
+        Vector<String> names = new Vector<>();
         names.add("ID");
         names.add("Type");
         names.add("First name");
@@ -363,11 +247,17 @@ public class UserList extends JFrame {
         UserType[] types = UserType.values();
         JComboBox typeCombo = new JComboBox(types);
         final DefaultCellEditor editor = new DefaultCellEditor(typeCombo);
-        table = new JTable(new T(names, users));
-        /*table = new JTable(new DefaultTableModel(
-                users,
-                names
-        )) {
+        table = new JTable(new UserListTableModel(names, new Vector<Vector<Object>>())) {
+            @Override
+            public TableCellEditor getCellEditor(int row, int column) {
+                int modelColumn = convertColumnIndexToModel(column);
+
+                if (modelColumn == 1)
+                    return editor;
+                else
+                    return super.getCellEditor(row, column);
+            }
+
             @Override
             public Class getColumnClass(int column) {
                 switch (column) {
@@ -387,51 +277,9 @@ public class UserList extends JFrame {
                         return String.class;
                 }
             }
-
-            @Override
-            public TableCellEditor getCellEditor(int row, int column) {
-                int modelColumn = convertColumnIndexToModel(column);
-
-                if (modelColumn == 1)
-                    return editor;
-                else
-                    return super.getCellEditor(row, column);
-            }
-
-            @Override
-            public int getColumnCount() {
-                System.out.println(names.size());
-                return names.size();
-            }
-
-            @Override
-            public int getRowCount() {
-                System.out.println(users.size());
-                return users.size();
-            }
-
-            @Override
-            public String getColumnName(int col) {
-                return names.get(col);
-            }
-
-            @Override
-            public Object getValueAt(int row, int col) {
-                System.out.println(users.get(row).get(col));
-                return users.get(row).get(col);
-            }
-
-            @Override
-            public void setValueAt(Object value, int row, int col) {
-                //System.out.print(getValueAt(row, col) + "   ");
-                users.get(row).set(col, value);
-                fireTableCellUpdated(row, col);
-                //System.out.println(getValueAt(row, col));
-            }
-        };*/
+        };
         TableRowSorter<TableModel> sorter
-                = new TableRowSorter<TableModel>(table.getModel());
-        //table.setAutoCreateRowSorter(true);
+                = new TableRowSorter<>(table.getModel());
         table.setRowSorter(sorter);
         add(new JScrollPane(table));
     }
