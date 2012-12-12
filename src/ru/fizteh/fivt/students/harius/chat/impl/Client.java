8db34d05fc1809ea72bfc297a0af9fbaadc0795d
Zakhar Pilipchuk <z.pilipchuk@gmail.com>
2012-12-12 14:30:31 +0400

    Structure formed

--- src/ru/fizteh/fivt/students/harius/chat/impl/Client.java
+++ src/ru/fizteh/fivt/students/harius/chat/impl/Client.java
@@ -3,9 +3,48 @@ package ru.fizteh.fivt.students.harius.chat.impl;
 import ru.fizteh.fivt.students.harius.chat.base.*;
 
 public final class Client {
-    private DisplayBase display;
+    private final DisplayBase display;
+    private ClientConsoleAdapter inputProcessor
+        = new ClientConsoleAdapter() {
+        
+        @Override
+        public void connect(String host, int port) {
+
+        }
+
+        @Override
+        public void disconnect() {
+
+        }
+
+        @Override
+        public void whereami() {
+
+        }
+
+        @Override
+        public void list() {
+
+        }
+
+        @Override
+        public void use(String server) {
+
+        }
+
+        @Override
+        public void exit() {
+
+        }
+
+        @Override
+        public void sendMessage(String message) {
+
+        }
+    };
 
     public Client(DisplayBase display) {
         this.display = display;
+        display.setObserver(inputProcessor);
     }
 }
--- src/ru/fizteh/fivt/students/harius/chat/impl/Server.java
+++ src/ru/fizteh/fivt/students/harius/chat/impl/Server.java
@@ -3,9 +3,48 @@ package ru.fizteh.fivt.students.harius.chat.impl;
 import ru.fizteh.fivt.students.harius.chat.base.*;
 
 public final class Server {
-    private DisplayBase display;
+    private final DisplayBase display;
+    private ServerConsoleAdapter inputProcessor
+        = new ServerConsoleAdapter() {
+        
+        @Override
+        public void listen(int port) {
+
+        }
+
+        @Override
+        public void stop() {
+
+        }
+
+        @Override
+        public void list() {
+
+        }
+
+        @Override
+        public void send(String user, String message) {
+
+        }
+
+        @Override
+        public void sendall(String message) {
+
+        }
+
+        @Override
+        public void kill(String user) {
+
+        }
+
+        @Override
+        public void exit() {
+
+        }
+    };
 
     public Server(DisplayBase display) {
         this.display = display;
+        display.setObserver(inputProcessor);
     }
 }
