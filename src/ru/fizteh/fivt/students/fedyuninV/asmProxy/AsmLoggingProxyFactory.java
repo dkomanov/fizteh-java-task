bccd9625e43bdc9d4e0cc628185d35ec01e323fa
FedyuninV <stelz40494@gmail.com>
2012-12-15 00:23:11 +0400

    Придется всеже через ga:(

---
+++ src/ru/fizteh/fivt/students/fedyuninV/asmProxy/AsmLoggingProxyFactory.java
@@ -0,0 +1,13 @@
+package ru.fizteh.fivt.students.fedyuninV.asmProxy;
+
+import ru.fizteh.fivt.proxy.LoggingProxyFactory;
+
+/**
+ * Fedyunin Valeriy
+ * MIPT FIVT 195
+ */
+public class AsmLoggingProxyFactory implements LoggingProxyFactory{
+    @Override
+    public Object createProxy(Object target, Appendable writer, Class... interfaces) {
+    }
+}
