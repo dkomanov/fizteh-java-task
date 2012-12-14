a9e77b47127d567ba238369f7d9f8581d8a563d8
Timin Alexander <altimin@altimin.ru>
2012-12-15 00:23:15 +0400

    xmlBinder fix: add <Objects> support

--- src/ru/fizteh/fivt/students/altimin/binder/XmlBinderTest.java
+++ src/ru/fizteh/fivt/students/altimin/binder/XmlBinderTest.java
@@ -1,10 +1,15 @@
-package ru.fizteh.fivt.students.altimin.xmlbinder;
+package ru.fizteh.fivt.students.altimin.binder;
 
+import org.junit.Test;
 import ru.fizteh.fivt.bind.test.Permissions;
 import ru.fizteh.fivt.bind.test.User;
 import ru.fizteh.fivt.bind.test.UserName;
 import ru.fizteh.fivt.bind.test.UserType;
-import ru.fizteh.fivt.students.altimin.xmlbinder.test.A;
+import ru.fizteh.fivt.students.altimin.binder.XmlBinder;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
 
 /**
  * User: altimin
@@ -12,22 +17,33 @@ import ru.fizteh.fivt.students.altimin.xmlbinder.test.A;
  * Time: 3:14 PM
  */
 public class XmlBinderTest {
-    public static void main(String[] args) {
+    @Test
+    public void testUser() {
         XmlBinder<User> binder = new XmlBinder<User>(User.class);
         Permissions permissions = new Permissions();
         permissions.setQuota(100500);
         User user = new User(1, UserType.USER, new UserName("first", "last"), permissions);
         byte[] bytes = binder.serialize(user);
         User deserialized = binder.deserialize(bytes);
-        assert user != deserialized;
-        assert user.equals(deserialized);
-        XmlBinder<A> binderA = new XmlBinder<A>(A.class);
-        A a = new A(4, 5);
-        bytes = binderA.serialize(a);
-        System.out.println(new String(bytes));
-        A deserializedA = binderA.deserialize(bytes);
-        assert deserializedA != a;
-        assert deserializedA.equals(a);
-        System.out.println("OK");
+        assertTrue(user != deserialized);
+        assertEquals(user, deserialized);
+    }
+
+    @Test
+    public void testUsers() {
+        XmlBinder<User> binder = new XmlBinder<User>(User.class);
+        Permissions permissions = new Permissions();
+        permissions.setQuota(100500);
+        User user1 = new User(1, UserType.USER, new UserName("first", "last"), permissions);
+        Permissions permissions2 = new Permissions();
+        permissions2.setQuota(500100);
+        User user2 = new User(2, UserType.MODERATOR, new UserName("a", "b"), permissions);
+        byte[] bytes = binder.serialize(user1, user2);
+        User[] deserialized = binder.deserializeObjects(bytes);
+        assertTrue(deserialized.length == 2);
+        assertTrue(user1 != deserialized[0]);
+        assertTrue(user2 != deserialized[1]);
+        assertEquals(user1, deserialized[0]);
+        assertEquals(user2, deserialized[1]);
     }
 }
