0f6727eb261276811c8880a58d7378f0f035462f
Zakhar Pilipchuk <z.pilipchuk@gmail.com>
2012-12-15 17:59:56 +0400

    Coding fast!

---
+++ src/ru/fizteh/fivt/students/harius/chat/io/ProtocolException.java
@@ -0,0 +1,7 @@
+package ru.fizteh.fivt.students.harius.chat.io;
+
+public class ProtocolException extends RuntimeException {
+    public ProtocolException(String message) {
+        super(message);
+    }
+}
