d87925aa939051f85fdd08aeba6b37bd2d49fe4e
Zakhar Pilipchuk <z.pilipchuk@gmail.com>
2012-12-16 18:55:57 +0400

    Renamed classes due to Code Conventions

--- src/ru/fizteh/fivt/students/harius/chat/impl/displays/Gui.java
+++ src/ru/fizteh/fivt/students/harius/chat/impl/displays/Gui.java
@@ -8,7 +8,7 @@ package ru.fizteh.fivt.students.harius.chat.impl.displays;
 
 import ru.fizteh.fivt.students.harius.chat.base.DisplayBase;
 
-public class GUI extends DisplayBase
+public class Gui extends DisplayBase
 {
     @Override
     public void message(String user, String message) {
--- src/ru/fizteh/fivt/students/harius/chat/launch/LaunchClientGui.java
+++ src/ru/fizteh/fivt/students/harius/chat/launch/LaunchClientGui.java
@@ -7,18 +7,18 @@
 package ru.fizteh.fivt.students.harius.chat.launch;
 
 import ru.fizteh.fivt.students.harius.chat.impl.Client;
-import ru.fizteh.fivt.students.harius.chat.impl.displays.GUI;
+import ru.fizteh.fivt.students.harius.chat.impl.displays.Gui;
 
-public class LaunchClientGUI {
+public class LaunchClientGui {
     public static void main(String[] args) {
         if (args.length != 1) {
             System.err.println("Please provide your nickname");
         } else {
-            new LaunchClientGUI().launch(args[0]);
+            new LaunchClientGui().launch(args[0]);
         }
     }
 
     public void launch(String name) {
-        new Client(new GUI(), name);
+        new Client(new Gui(), name);
     }
 }
