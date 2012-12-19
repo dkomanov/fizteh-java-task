dfc25839988f10ee56f671a2b428c4e33249a1de
Nikolay Frolov <frolov.nikolay@phystech.edu>
2012-12-19 18:25:37 +0400

    Хочется закрыть чат.

--- src/ru/fizteh/fivt/students/frolovNikolay/chat/MsgHandler.java
+++ src/ru/fizteh/fivt/students/frolovNikolay/chat/MsgHandler.java
@@ -3,29 +3,54 @@ package ru.fizteh.fivt.students.frolovNikolay.chat;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
+import java.util.List;
 
 import ru.fizteh.fivt.chat.MessageType;
 
 public class MsgHandler {
     
-    static public String parseHelloAndError(byte[] msg) {
-        byte[] inf = new byte[msg.length - 1];
-        for (int i = 1; i < msg.length; ++i) {
-            inf[i - 1] = msg[i];
+    static public String parseHelloAndError(List<Byte> byteBuffer) {
+        byte[] inf = new byte[byteBuffer.size() - 1];
+        for (int i = 1; i < byteBuffer.size(); ++i) {
+            inf[i - 1] = byteBuffer.get(i);
         }
+        byteBuffer.clear();
         return new String(inf).trim();
     }
     
-    static public String[] parseMessage(byte[] msg) {
+    static public String[] parseMessage(List<Byte> byteBuffer) {
         try {
             String[] result = new String[2];
-            StringBuffer message = new StringBuffer();
-            ByteBuffer buffer = ByteBuffer.wrap(msg);
-            buffer.get();
-            int numberOfMessages = buffer.get();
+            StringBuilder message = new StringBuilder();
+            int position = 1;
+            if (byteBuffer.size() < 2) {
+                return null;
+            }
+            int numberOfMessages = byteBuffer.get(position++);
+            if (numberOfMessages < 0) {
+                throw new RuntimeException("Incorrect number of messages");
+            }
             for (int i = 0; i < numberOfMessages; ++i) {
-                byte[] bytes = new byte[buffer.getInt()];
-                buffer.get(bytes);
+                if (i + 4 + position > byteBuffer.size()) {
+                    return null;
+                }
+                byte[] byteSize = new byte[4];
+                for (int j = 0; j < 4; ++j) {
+                    byteSize[j] = byteBuffer.get(position + j);
+                }
+                position += 4;
+                int size = ((byteSize[0] & 0xFF) << 24) + ((byteSize[1] & 0xFF) << 16) + ((byteSize[2] & 0xFF) << 8) + (byteSize[3] & 0xFF);
+                if (size <= 0) {
+                    throw new RuntimeException("Incorrect length of message");
+                }
+                if (position + size > byteBuffer.size()) {
+                    return null;
+                }
+                byte[] bytes = new byte[size];
+                for (int j = 0; j < size; ++j) {
+                    bytes[j] = byteBuffer.get(position + j);
+                }
+                position += size;
                 if (i != 0) {
                     message.append(new String(bytes));
                 } else {
@@ -33,6 +58,13 @@ public class MsgHandler {
                 }
             }
             result[1] = message.toString().trim();
+            if (position == byteBuffer.size()) {
+                byteBuffer.clear();
+            } else {
+                for (int i = 0; i < position; ++i) {
+                    byteBuffer.remove(0);
+                }
+            }
             return result;
         } catch (Throwable ignoringException) {
             String[] result = new String[2];
--- src/ru/fizteh/fivt/students/frolovNikolay/chat/server/Server.java
+++ src/ru/fizteh/fivt/students/frolovNikolay/chat/server/Server.java
@@ -23,6 +23,8 @@ public class Server {
     private static String serverName = "<server>";
     private int port = -1;
     private HashMap<SocketChannel, String> clientsNameMapping = new HashMap<SocketChannel, String>();
+    private HashMap<SocketChannel, List<Byte>> clientsBuffer = new HashMap<SocketChannel, List<Byte>>();
+    private HashMap<SocketChannel, Integer> badTries = new HashMap<SocketChannel, Integer>();
     private ServerSocketChannel serverSocket;
     private Selector multiplexor;
     private List<SocketChannel> noNameClients = new ArrayList<SocketChannel>();
@@ -60,6 +62,8 @@ public class Server {
         } else {
             noNameClients.remove(clientSock);
         }
+        clientsBuffer.remove(clientSock);
+        badTries.remove(clientSock);
         Closer.close(clientSock);
         if (nickName != null) {
             sendAll(nickName + " has left the chat");
@@ -76,34 +80,45 @@ public class Server {
             if (ChatUtils.hasKey(key, SelectionKey.OP_ACCEPT)) { 
                 SocketChannel clientSock = serverSocket.accept();
                 if (clientSock != null) {
-                    System.out.println("connected");
                     clientSock.configureBlocking(false);
                     clientSock.register(multiplexor, SelectionKey.OP_READ);
                     noNameClients.add(clientSock);
+                    clientsBuffer.put(clientSock, new ArrayList<Byte>());
+                    badTries.put(clientSock, new Integer(0));
                 }                    
             } else if (ChatUtils.hasKey(key, SelectionKey.OP_READ)) {
                 ByteBuffer buffer = ByteBuffer.allocate(1024);
                 SocketChannel clientSock = (SocketChannel) key.channel();
-                if (clientSock.read(buffer) != -1) {
-                    byte[] msg = buffer.array();
-                    switch (msg[0]) {
+                int state;
+                while ((state = clientSock.read(buffer)) > 0) {
+                    byte[] piece = buffer.array();
+                    for (int i = 0; i < piece.length; ++i) {
+                        clientsBuffer.get(clientSock).add(piece[i]);
+                    }
+                }
+                if (state != -1) {
+                    switch (clientsBuffer.get(clientSock).get(0)) {
                     
                         // Hello message
                         case 1: {
                             System.out.println("have hello");
-                            String nickName = MsgHandler.parseHelloAndError(msg);
+                            String nickName = MsgHandler.parseHelloAndError(clientsBuffer.get(clientSock));
                             boolean badNick = false;
                             if (nickName == null || nickName.isEmpty()) {
                                 badNick = true;
+                                System.err.println("bad1");
                                 ChatUtils.sendMsg(MsgHandler.error("Incorrect msg"), clientSock);
                             } else if (nickName.length() > 50) {
                                 badNick = true;
+                                System.err.println("bad2");
                                 ChatUtils.sendMsg(MsgHandler.error("Too long nickname(can have a maximum 50 characters)"), clientSock);
                             } else if (nickName.equals("<server>") || clientsNameMapping.containsKey(nickName)) {
+                                System.err.println("bad3");
                                 badNick = true;
                                 ChatUtils.sendMsg(MsgHandler.error("Someone already has this nickname"), clientSock);
                             }
                             if (badNick) {
+                                System.err.println("bad");
                                 ChatUtils.sendMsg(MsgHandler.bye(), clientSock);
                                 Closer.close(clientSock);
                             } else {
@@ -126,7 +141,14 @@ public class Server {
                                 ChatUtils.sendMsg(MsgHandler.error("You haven't authorized"), clientSock);
                                 continue;
                             }
-                            String[] parsedMsg = MsgHandler.parseMessage(msg);
+                            String[] parsedMsg = MsgHandler.parseMessage(clientsBuffer.get(clientSock));
+                            if (parsedMsg == null) {
+                                badTries.put(clientSock, new Integer(badTries.get(clientSock) + 1));
+                                if (badTries.get(clientSock) == 10000) {
+                                    disconnectClient(clientSock);
+                                }
+                                break;
+                            }
                             if (!isMsgCorrect(parsedMsg[0], parsedMsg[1], clientSock)) {
                                 continue;
                             }
@@ -158,8 +180,9 @@ public class Server {
                                 ChatUtils.sendMsg(MsgHandler.error("You haven't authorized"), clientSock);
                                 continue;
                             }
-                            String errorMsg = MsgHandler.parseHelloAndError(msg);
+                            String errorMsg = MsgHandler.parseHelloAndError(clientsBuffer.get(clientSock));
                             System.out.println(nickName + " sended error message: " + errorMsg);
+                            disconnectClient(clientSock);
                             break;
                         }
 
@@ -168,13 +191,13 @@ public class Server {
                             String nickName = null;
                             if (clientsNameMapping.containsKey(clientSock)) {
                                 nickName = clientsNameMapping.get(clientSock);
+                                System.out.println("Unknown type message from: " + nickName);
+                                ChatUtils.sendMsg(MsgHandler.error("Incorrect message"), clientSock);
                             } else {
                                 System.err.println("Someone unauthorized is trying to sengMsg");
                                 ChatUtils.sendMsg(MsgHandler.error("You haven't authorized"), clientSock);
-                                continue;
                             }
-                            System.out.println("Unknown type message from: " + nickName);
-                            ChatUtils.sendMsg(MsgHandler.error("Incorrect message"), clientSock);
+                            disconnectClient(clientSock);
                         }
                     }
                 } else {
@@ -213,6 +236,8 @@ public class Server {
         }
         Closer.close(serverSocket);
         clientsNameMapping.clear();
+        clientsBuffer.clear();
+        badTries.clear();
     }
 
     private SocketChannel getSocketByName(String userName) {
